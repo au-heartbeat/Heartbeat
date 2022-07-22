fbb5b5c3692c1c32f545938da9433c661b666139
laceyhong <haxz003@163.com>
2022-07-22 16:01:30 +0800

    feat: get all sprints data

--- backend/tests/services/kanban/Jira/Jira.test.ts
+++ backend/tests/services/kanban/Jira/Jira.test.ts
@@ -4,9 +4,11 @@ import sinon from "sinon";
 import { Jira } from "../../../../src/services/kanban/Jira/Jira";
 import { mock } from "../../../TestTools";
 import JiraCards from "../../../fixture/JiraCards.json";
+import Sprints from "../../../fixture/Sprints.json";
 import JiraCardCycleTime from "../../../fixture/JiraCardCycleTime.json";
 import { StoryPointsAndCycleTimeRequest } from "../../../../src/contract/kanban/KanbanStoryPointParameterVerify";
 import { CycleTimeInfo } from "../../../../src/contract/kanban/KanbanStoryPointResponse";
+import { Sprint } from "../../../../src/models/kanban/Sprint";
 
 const jira = new Jira("testToken", "domain");
 
@@ -29,6 +31,36 @@ describe("get story points and cycle times of done cards during period", () => {
     ],
     false
   );
+
+  it("should return all the sprints by domain name and borad id", async () => {
+    const expected = [
+      new Sprint(
+        1,
+        "closed",
+        "ADM Sprint 1",
+        "2020-05-26T03:20:43.632Z",
+        "2020-06-09T03:20:37.000Z",
+        "2020-07-22T01:46:20.917Z"
+      ),
+      new Sprint(
+        4,
+        "closed",
+        "ADM Sprint 2",
+        "2020-07-22T01:48:39.455Z",
+        "2020-08-05T01:48:37.000Z",
+        "2020-07-22T01:49:26.508Z"
+      ),
+      new Sprint(9, "future", "ADM Sprint 5"),
+    ];
+    mock
+      .onGet(
+        `https://${storyPointsAndCycleTimeRequest.site}.atlassian.net/rest/agile/1.0/board/${storyPointsAndCycleTimeRequest.boardId}/sprint`
+      )
+      .reply(200, Sprints);
+    expect(
+      await jira.getAllSprintsByBoardId(storyPointsAndCycleTimeRequest)
+    ).deep.equal(expected);
+  });
 
   it("should return story points when having matched cards", async () => {
     sinon.stub(Jira, "getCycleTimeAndAssigneeSet").returns(
