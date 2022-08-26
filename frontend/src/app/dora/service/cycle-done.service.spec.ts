0c5d89cb0a39f102db4d7c127797c92777832f52
chaoyangyes <chao.yang1@thoughtworks.com>
2022-08-26 14:03:41 +0800

    test: add test in cycle-done.service file

--- frontend/src/app/dora/service/cycle-done.service.spec.ts
+++ frontend/src/app/dora/service/cycle-done.service.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 
 import { CycleDoneService } from './cycle-done.service';
 
 describe('CycleDoneService', () => {
-  let service: CycleDoneService;
+  let cycleDoneService: CycleDoneService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CycleDoneService);
+    cycleDoneService = TestBed.inject(CycleDoneService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(cycleDoneService).toBeTruthy();
   });
+
+  it('should set a new value and then can get the same value', async(() => {
+    const testValue: string[] = ['ADM-212'];
+
+    cycleDoneService.setValue(testValue);
+    cycleDoneService.getValue().subscribe((res) => {
+      expect(res).toEqual(testValue);
+    });
+  }));
 });
