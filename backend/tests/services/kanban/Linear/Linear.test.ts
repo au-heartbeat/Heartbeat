c9500a1bde66ce13ff15a627cdbe658875b9d9ae
Zhaosen Wen <zhaosen.wen@thoughtworks.com>
2022-12-16 14:24:26 +0800

    refactor: delete extra log in test.

--- backend/tests/services/kanban/Linear/Linear.test.ts
+++ backend/tests/services/kanban/Linear/Linear.test.ts
@@ -68,7 +68,6 @@ describe("get story points and cycle times of done cards during period", () => {
       ["test"]
     );
     expect(response.storyPointSum).equal(3);
-    logger.info(response.matchedCards[0].cycleTime);
     expect(response.matchedCards[0].cycleTime).deep.equal([
       {
         column: "IN PROGRESS",
