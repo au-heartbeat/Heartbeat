42c209111695d9efe065f40b0609df5774061953
zhongren.gu <zhongren.gu@thoughtworks.com>
2022-12-22 21:16:33 +0800

    test: fix test prefix

--- backend/tests/controller/HealthController.test.ts
+++ backend/tests/controller/HealthController.test.ts
@@ -3,6 +3,7 @@ import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import app from "../../src/server";
 import sinon from "sinon";
+import { API_VERSION } from "../fixture/common";
 
 chai.use(chaiHttp);
 chai.should();
@@ -11,9 +12,9 @@ describe("HealthController", () => {
   describe("Check health endpoint", () => {
 
     it("should return 200 and true(response) when request health endpoint", async () => {
-    const baseUrl = "/health";
+      const HEALTH_CHECK_URL = `${API_VERSION}/health`;
 
-      const response = await chai.request(app).get(baseUrl);
+      const response = await chai.request(app).get(HEALTH_CHECK_URL);
 
       expect(response.status).equal(200);
       expect(response.body).deep.equal(true);
