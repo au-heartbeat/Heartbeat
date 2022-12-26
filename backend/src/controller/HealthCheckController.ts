2b0605a0891c73ee4481c80e84be3c46ac4e1509
zhongren.gu <zhongren.gu@thoughtworks.com>
2022-12-26 13:58:02 +0800

    fix: update classname to healthckeck

--- backend/src/controller/HealthCheckController.ts
+++ backend/src/controller/HealthCheckController.ts
@@ -2,7 +2,7 @@ import { description, request, summary, tagsAll } from "koa-swagger-decorator";
 import { Context } from "koa-swagger-decorator/dist";
 
 @tagsAll(["Health Check"])
-export default class CodebaseController {
+export default class HealthCheckController {
   @request("get", "/health")
   @summary("Check app's status")
   @description("Check app's status")
