e1312a283854c0e753bf9169b4d90819b66eb272
Eason-T <awawerwer2007@163.com>
2023-03-23 09:30:10 +0800

    ADM-375.1 fix frontend field

--- backend/src/main/java/heartbeat/controller/board/vo/request/BoardType.java
+++ backend/src/main/java/heartbeat/controller/board/vo/request/BoardType.java
@@ -2,7 +2,7 @@ package heartbeat.controller.board.vo.request;
 
 public enum BoardType {
 
-	JIRA("jira"), CLASSIC_JIRA("classic_jira");
+	JIRA("jira"), CLASSIC_JIRA("classic-jira");
 
 	public final String boardType;
 
