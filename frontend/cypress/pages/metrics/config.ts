dc88d4f60ad9aec2fcde320e0ddfb60ca3f8160b
Ting Wang <ting.wang@thoughtworks.com>
2023-03-27 13:38:54 +0800

    ADM-375.1 add e2e test for board component when select classic jira

--- frontend/cypress/pages/metrics/config.ts
+++ frontend/cypress/pages/metrics/config.ts
@@ -15,16 +15,39 @@ class Config {
   }
 
   selectDateRange() {
-    cy.contains('From').parent().type('02052023')
+    cy.get("[data-testid='CalendarIcon']").first().click()
+    cy.contains('16').click()
   }
 
   selectMetricsData() {
     cy.contains('Required Data').siblings().click()
 
+    cy.contains('Velocity').click()
     cy.contains('Lead time for changes').click()
     cy.contains('Deployment frequency').click()
 
     cy.get('div.MuiBackdrop-root.MuiBackdrop-invisible.MuiModal-backdrop').click({ force: true })
+  }
+
+  fillBoardInfoAndVerifyWithClassicJira(
+    boardId: string,
+    email: string,
+    projectKey: string,
+    site: string,
+    token: string
+  ) {
+    cy.contains('Jira').click()
+    cy.contains('Classic Jira').click()
+
+    cy.contains('Board Id').siblings().first().type(boardId)
+    cy.contains('Email').siblings().first().type(email)
+    cy.contains('Project Key').siblings().first().type(projectKey)
+    cy.contains('Site').siblings().first().type(site)
+    cy.contains('Token').siblings().first().type(token)
+
+    cy.contains('Verify').click()
+    cy.contains('Verified').should('exist')
+    cy.contains('Reset').should('exist')
   }
 
   fillPipelineToolFieldsInfoAndVerify(token: string) {
