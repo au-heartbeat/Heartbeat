361645381df3838564fe803b084f1fbc9885422c
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-04-04 17:40:13 +0800

    ADM-356 Add verification for e2e

--- frontend/cypress/e2e/createANewProject.cy.ts
+++ frontend/cypress/e2e/createANewProject.cy.ts
@@ -45,9 +45,21 @@ describe('Create a new project', () => {
 
     homePage.importProjectFromFile()
     cy.url().should('include', '/metrics')
+    cy.contains('Pipeline Tool').should('exist')
+    cy.get('.MuiInput-input')
+      .invoke('val')
+      .then((value) => {
+        expect(value).to.equal('mockProjectName')
+      })
 
     configPage.verifyAndClickNextToMetrics()
 
     configPage.goMetricsStep()
+    cy.contains('Classification Setting').should('exist')
+    cy.get('.MuiSelect-select')
+      .invoke('text')
+      .then((text) => {
+        expect(text).to.equal('Yu ZhangTo doAnalysis------------------------​Parent')
+      })
   })
 })
