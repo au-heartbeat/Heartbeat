3a063143e94e0ec991860d655a1b16f1c9d30f2c
shawn he <64237577+spadio9587@users.noreply.github.com>
2023-04-06 15:39:16 +0800

    ADM-369 add CycleTimeReport in Report Page

---
+++ frontend/src/components/Metrics/ReportStep/CycleTime/index.tsx
@@ -0,0 +1,76 @@
+import MetricsSettingTitle from '@src/components/Common/MetricsSettingTitle'
+import { TableBody, TableCell, TableHead, TableRow, Table } from '@mui/material'
+import { Container, Row } from '@src/components/Metrics/ReportStep/style'
+import { CycleTime } from '@src/types/reportResponse'
+
+interface CycleTimeProps {
+  title: string
+  cycleTimeData: CycleTime
+}
+
+export const CycleTimeReport = ({ title, cycleTimeData }: CycleTimeProps) => {
+  const updatedSwimlaneList = cycleTimeData.swimlaneList
+    .map((item) => ({
+      ...item,
+      optionalItemName:
+        item.optionalItemName === 'Waiting for testing'
+          ? 'Waiting'
+          : item.optionalItemName === 'In Dev'
+          ? 'Development'
+          : item.optionalItemName,
+    }))
+    .filter((item) => !['Analysis', 'Done'].includes(item.optionalItemName))
+
+  const averageTimeProportion = updatedSwimlaneList.map((item) => ({
+    name: item.optionalItemName,
+    value: (parseFloat(item.totalTime) / cycleTimeData.totalTimeForCards).toFixed(2),
+  }))
+
+  const averageTime = updatedSwimlaneList.map((item) => ({
+    name: item.optionalItemName,
+    averageTimeForSp: item.averageTimeForSP,
+    averageTimeForCards: item.averageTimeForCards,
+  }))
+
+  return (
+    <>
+      <MetricsSettingTitle title={title} />
+      <Container>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align={'inherit'}>Value</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell rowSpan={2}>Average Cycle Time</TableCell>
+              <TableCell align='inherit'>{cycleTimeData.averageCycleTimePerSP}(days/SP)</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align='inherit'>{cycleTimeData.averageCircleTimePerCard}(days/card)</TableCell>
+            </TableRow>
+            {averageTimeProportion.map(({ name, value }) => (
+              <Row key={name}>
+                <TableCell align='inherit'>Total {name} Time / Total Cycle Time </TableCell>
+                <TableCell align='inherit'>{value}</TableCell>
+              </Row>
+            ))}
+            {averageTime.map(({ name, averageTimeForCards, averageTimeForSp }) => (
+              <>
+                <TableRow>
+                  <TableCell rowSpan={2}>Average {name} Time</TableCell>
+                  <TableCell align='inherit'>{averageTimeForSp}(days/SP)</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align='inherit'>{averageTimeForCards}(days/card)</TableCell>
+                </TableRow>
+              </>
+            ))}
+          </TableBody>
+        </Table>
+      </Container>
+    </>
+  )
+}
