a4ab64659235870d9ca9b49be58d1443fdba6113
fengxinhou <fengxin.hou@thoughtworks.com>
2023-04-12 10:07:55 +0800

    ADM-438 change the way of classification's values mapper

--- frontend/src/mapper/ClassificationMapper.ts
+++ frontend/src/mapper/ClassificationMapper.ts
@@ -5,10 +5,16 @@ export const classificationMapper = (classification: ClassificationResp[]) => {
   const mappedClassificationValue: ReportDataWithThreeColumns[] = []
 
   classification.map((item) => {
+    const pairsValues: { name: string; value: string }[] = []
+
+    item.pairs.map((pairItem) => {
+      pairsValues.push({ name: pairItem.name, value: pairItem.value })
+    })
+
     const classificationValue: ReportDataWithThreeColumns = {
       id: mappedClassificationValue.length + 1,
       name: item.fieldName,
-      values: item.pairs,
+      values: pairsValues,
     }
     mappedClassificationValue.push(classificationValue)
   })
