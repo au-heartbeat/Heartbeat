8d2b4ed37960e960d2a110e2f4f53ed3149d23e5
fengxinhou <fengxin.hou@thoughtworks.com>
2023-04-12 13:56:07 +0800

    ADM-438 add test for change failure rate data mapper

---
+++ frontend/__tests__/src/mapper/changeFailureRateMapper.test.ts
@@ -0,0 +1,44 @@
+import { changeFailureRateMapper } from '@src/mapper/ChangeFailureRateMapper'
+
+describe('change failure rate data mapper', () => {
+  const mockChangeFailureRateRes = {
+    avgChangeFailureRate: {
+      name: 'Average',
+      failureRate: '0.00% (0/12)',
+    },
+    changeFailureRateOfPipelines: [
+      {
+        name: 'fs-platform-onboarding',
+        step: ' :shipit: deploy to PROD',
+        failureRate: '0.00% (0/3)',
+      },
+    ],
+  }
+  it('maps response change failure rate values to ui display value', () => {
+    const expectedChangeFailureRateValues = [
+      {
+        id: 0,
+        name: 'fs-platform-onboarding/ :shipit: deploy to PROD',
+        values: [
+          {
+            name: 'Failure Rate',
+            value: '0.00% (0/3)',
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: 'Average/',
+        values: [
+          {
+            name: 'Failure Rate',
+            value: '0.00% (0/12)',
+          },
+        ],
+      },
+    ]
+    const mappedChangeFailureRate = changeFailureRateMapper(mockChangeFailureRateRes)
+
+    expect(mappedChangeFailureRate).toEqual(expectedChangeFailureRateValues)
+  })
+})
