3a2e50e11b6cfc4199dd28d01b5f400372ba85ef
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-04-13 17:42:23 +0800

    ADM-370 delete unuseful component and test files

--- frontend/src/components/Metrics/ReportStep/index.tsx
+++ frontend/src/components/Metrics/ReportStep/index.tsx
@@ -1,17 +1,30 @@
-import { Velocity } from '@src/components/Metrics/ReportStep/Velocity'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGenerateReportEffect } from '@src/hooks/useGenerateReportEffect'
 import { Loading } from '@src/components/Loading'
 import { useAppSelector } from '@src/hooks'
 import { selectConfig } from '@src/context/config/configSlice'
-import { INIT_REPORT_DATA_WITH_TWO_COLUMNS } from '@src/constants'
-import { CycleTimeReport } from '@src/components/Metrics/ReportStep/CycleTime'
+import { reportResponseMapper } from '@src/mapper/ReportMapper'
+import {
+  INIT_CYCLETIME_METRICS,
+  INIT_VELOCITY_METRICS,
+  INIT_CLASSIFICATION_METRICS,
+  NAME,
+  PIPELINE_STEP,
+  INIT_DEPLOYMENT_METRICS,
+  INIT_LEAD_TIME_FOR_CHANGES_METRICS,
+  CHANGE_FAILURE_RATE_METRICS,
+} from '@src/constants'
+import ReportForThreeColumns from '@src/components/Common/ReportForThreeColumns'
+import ReportForTwoColumns from '@src/components/Common/ReportForTwoColumns'
 
 export const ReportStep = () => {
   const { generateReport, isLoading } = useGenerateReportEffect()
-  const [velocityData, setVelocityData] = useState(INIT_REPORT_DATA_WITH_TWO_COLUMNS)
-  const [cycleTimeData, setCycleTimeData] = useState(INIT_REPORT_DATA_WITH_TWO_COLUMNS)
-  const [classificationData, setClassificationData] = useState(INIT_REPORT_DATA_WITH_THREE_COLUMNS)
+  const [velocityData, setVelocityData] = useState(INIT_VELOCITY_METRICS)
+  const [cycleTimeData, setCycleTimeData] = useState(INIT_CYCLETIME_METRICS)
+  const [classificationData, setClassificationData] = useState(INIT_CLASSIFICATION_METRICS)
+  const [deploymentFrequencyData, setDeploymentFrequencyData] = useState(INIT_DEPLOYMENT_METRICS)
+  const [leadTimeForChangesData, setLeadTimeForChangesData] = useState(INIT_LEAD_TIME_FOR_CHANGES_METRICS)
+  const [changeFailureRateData, setChangeFailureRateData] = useState(CHANGE_FAILURE_RATE_METRICS)
   const configData = useAppSelector(selectConfig)
   const { metrics, calendarType, dateRange } = configData.basic
   const { board, pipelineTool, sourceControl } = configData
@@ -27,9 +40,10 @@ export const ReportStep = () => {
   useEffect(() => {
     generateReport(params).then((res) => {
       if (res) {
-        setVelocityData(res.velocityList)
-        setCycleTimeData(res.cycleTimeList)
-        setClassificationData(res.classificationList)
+        const reportData = reportResponseMapper(res.response)
+        setVelocityData(reportData.velocityValues)
+        setCycleTimeData(reportData.cycleValues)
+        setClassificationData(reportData.classificationValues)
       }
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -40,9 +54,32 @@ export const ReportStep = () => {
         <Loading />
       ) : (
         <>
-          <Velocity title={'Velocity'} velocityData={velocityData} />
-          <CycleTimeReport title={'Cycle time'} cycleTimeData={cycleTimeData} />
-          <Classification title={'Classification'} classificationData={classificationData} />
+          <ReportForTwoColumns title={'Velocity'} data={velocityData} />
+          <ReportForTwoColumns title={'Cycle time'} data={cycleTimeData} />
+          <ReportForThreeColumns
+            title={'Classification'}
+            fieldName='Field Name'
+            listName='Subtitle'
+            data={classificationData}
+          />
+          <ReportForThreeColumns
+            title={'Deployment Frequency'}
+            fieldName={PIPELINE_STEP}
+            listName={NAME}
+            data={deploymentFrequencyData}
+          />
+          <ReportForThreeColumns
+            title={'Lead Time For Change'}
+            fieldName={PIPELINE_STEP}
+            listName={NAME}
+            data={leadTimeForChangesData}
+          />
+          <ReportForThreeColumns
+            title={'Change Failure Rate'}
+            fieldName={PIPELINE_STEP}
+            listName={NAME}
+            data={changeFailureRateData}
+          />
         </>
       )}
     </>
