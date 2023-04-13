50749880ab68ce1dfcc6dad416dd996417420fe6
fengxinhou <fengxin.hou@thoughtworks.com>
2023-04-13 14:01:10 +0800

    ADM-438 refactor report mapper

--- frontend/src/clients/ReportClient.ts
+++ frontend/src/clients/ReportClient.ts
@@ -33,7 +33,6 @@ export class ReportClient extends HttpClient {
     try {
       await this.axiosInstance
         .post(
-          // `http://localhost:4323/api/v1/report`,
           `/report`,
           {},
           {
