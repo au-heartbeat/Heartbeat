43ad4874972e9514e9379f5e9cc9e6e1ad0715be
Lanxin Wang <lanxin.wang@thoughtworks.com>
2023-05-13 21:50:28 +0800

    ADM-464 add chromeWebSecurity false

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'cypress'
 
-console.log('-----------------:', process.env.APP_ORIGIN)
-
 export default defineConfig({
   videoCompression: 15,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       config.env.url = process.env.APP_ORIGIN || 'http://localhost:4321'
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('./cypress/plugins/updateConfigJson.ts')(on, config)
       return config
     },
+    chromeWebSecurity: false,
   },
 })
