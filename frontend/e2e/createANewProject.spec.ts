de97a164414011e580cf71803517043ed0d5f929
guzhongren <guzhongren@live.cn>
2023-05-27 21:47:09 +0800

    refactor(e2e): refactor with playwright

---
+++ frontend/e2e/createANewProject.spec.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@playwright/test'
+import dayjs from 'dayjs'
+
+import { HomePage } from './pages/Home'
+import { MetricsConfigPage } from './pages/metrics/config'
+
+test.use({
+  viewport: {
+    height: 1080,
+    width: 1920,
+  },
+})
+
+test('should create a project manually', async ({ page }) => {
+  const homePage = new HomePage(page)
+
+  await homePage.goto()
+  await homePage.createANewProject()
+
+  const metricsConfigPage = new MetricsConfigPage(page)
+
+  await metricsConfigPage.typeProjectName('test Project Name')
+
+  await expect(metricsConfigPage.collectionDate).toBeVisible()
+  await expect(metricsConfigPage.regularCalendar).toBeChecked()
+  await expect(metricsConfigPage.chinaCalendar).not.toBeChecked()
+
+  await metricsConfigPage.selectRegularCalendar()
+
+  const toDay = dayjs()
+  const fromDay = toDay.subtract(14, 'day')
+
+  await metricsConfigPage.selectDateRange(fromDay, toDay)
+
+  await metricsConfigPage.selectVelocityAndClassificationInRequireData()
+})
---
+++ frontend/e2e/pages/metrics/config.ts
@@ -0,0 +1,92 @@
+import { expect, Locator, Page } from '@playwright/test'
+import { Dayjs } from 'dayjs'
+export class MetricsConfigPage {
+  readonly page: Page
+  readonly projectNameInput: Locator
+  readonly RegularCalenderCheckbox: Locator
+  readonly projectNameErrorMessage: Locator
+  readonly collectionDate: Locator
+  readonly regularCalendar: Locator
+  readonly chinaCalendar: Locator
+  readonly chooseDateButton: Locator
+  readonly fromDateInput: Locator
+  readonly fromDateInputButton: Locator
+  readonly fromDateInputValueSelect: (fromDay: Dayjs) => Locator
+  readonly toDateInput: Locator
+  readonly toDateInputButton: Locator
+  readonly toDateInputValueSelect: (toDay: Dayjs) => Locator
+  readonly chooseDate: Locator
+  readonly requireDataButton: Locator
+  readonly velocityCheckbox: Locator
+  readonly classificationCheckbox: Locator
+  readonly requiredDataErrorMessage: Locator
+
+  constructor(page: Page) {
+    this.page = page
+    this.projectNameInput = page.locator('label', { hasText: 'Project Name *' })
+    this.projectNameErrorMessage = page.locator('Project Name is required')
+    this.collectionDate = page.locator('h3', { hasText: 'Collection Date' })
+    this.regularCalendar = page.locator("input[value='Regular Calendar(Weekend Considered)']")
+    this.chinaCalendar = page.locator("input[value='Calendar with Chinese Holiday']")
+    this.fromDateInput = page.getByRole('textbox', { name: 'From *' })
+    this.fromDateInputButton = page
+      .locator('div')
+      .filter({ hasText: /^From \*$/ })
+      .getByRole('button', { name: 'Choose date' })
+    this.fromDateInputValueSelect = (fromDay: Dayjs) =>
+      page.getByRole('dialog', { name: 'From *' }).getByRole('gridcell', { name: `${fromDay.date()}` })
+    this.toDateInput = page.getByRole('textbox', { name: 'To *' })
+    this.toDateInputButton = page
+      .locator('div')
+      .filter({ hasText: /^To \*$/ })
+      .getByRole('button', { name: 'Choose date' })
+    this.toDateInputValueSelect = (toDay: Dayjs) =>
+      page.getByRole('dialog', { name: 'To *' }).getByRole('gridcell', { name: `${toDay.date()}` })
+
+    this.requireDataButton = page.getByRole('button', { name: 'Required Data' })
+    this.velocityCheckbox = page.getByRole('option', { name: 'Velocity' }).getByRole('checkbox')
+    this.classificationCheckbox = page.getByRole('option', { name: 'Classification' }).getByRole('checkbox')
+    this.requiredDataErrorMessage = page.locator('Metrics is required')
+  }
+
+  async typeProjectName(projectName = 'e2e test name') {
+    await this.projectNameInput.type(projectName)
+  }
+
+  async selectRegularCalendar() {
+    await this.regularCalendar.click()
+
+    await expect(this.chinaCalendar).not.toBeChecked()
+    await expect(this.regularCalendar).toBeChecked()
+  }
+
+  async selectDateRange(fromDay: Dayjs, toDay: Dayjs) {
+    await this.fromDateInput.click()
+    await this.fromDateInputButton.click()
+    await this.fromDateInputValueSelect(fromDay).click()
+    expect(this.page.getByText(covertToDateString(fromDay))).toBeTruthy()
+
+    await this.toDateInputButton.click()
+    await this.toDateInputValueSelect(toDay).click()
+    expect(this.page.getByText(covertToDateString(toDay))).toBeTruthy()
+  }
+
+  async selectVelocityAndClassificationInRequireData() {
+    await this.requireDataButton.click()
+    await this.velocityCheckbox.check()
+    await this.classificationCheckbox.check()
+    await this.page.keyboard.press('Escape')
+
+    await expect(this.requireDataButton).toHaveText('Velocity, Classification')
+
+    await this.requireDataButton.click()
+    await this.velocityCheckbox.uncheck()
+    await this.classificationCheckbox.uncheck()
+    await this.page.keyboard.press('Escape')
+
+    expect(this.requiredDataErrorMessage).toBeTruthy()
+  }
+}
+function covertToDateString(day: Dayjs): string | RegExp {
+  return `${day.month() + 1} / ${day.date()} / ${day.year()}`
+}
