d2700e658246e6dfd605f2823891583a6af7b3fc
wangwang-zhang <112382558+wangwang-zhang@users.noreply.github.com>
2023-06-02 19:32:53 +0800

    Revert "Adm 373.4[frontend][backend]"

--- backend/src/main/java/heartbeat/service/report/CSVFileGenerator.java
+++ backend/src/main/java/heartbeat/service/report/CSVFileGenerator.java
@@ -1,13 +1,14 @@
 package heartbeat.service.report;
 
-import com.opencsv.CSVWriter;
 import heartbeat.controller.report.dto.response.LeadTimeInfo;
 import heartbeat.controller.report.dto.response.PipelineCSVInfo;
-import heartbeat.exception.FileIOException;
+import heartbeat.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import com.opencsv.CSVWriter;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -30,8 +31,7 @@ public class CSVFileGenerator {
 			return resource;
 		}
 		catch (IOException e) {
-			log.error("Failed to read file", e);
-			throw new FileIOException(e);
+			throw new NotFoundException(HttpStatus.NOT_FOUND.value(), "this file is not exist");
 		}
 	}
 
@@ -85,8 +85,7 @@ public class CSVFileGenerator {
 			}
 		}
 		catch (IOException e) {
-			log.error("Failed to write file", e);
-			throw new FileIOException(e);
+			throw new NotFoundException(HttpStatus.NOT_FOUND.value(), "this file is not exist");
 		}
 	}
 
