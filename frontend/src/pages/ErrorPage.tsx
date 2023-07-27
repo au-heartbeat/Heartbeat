695dad8f84421d73d91a5614f9ede0fc8393d37e
xingxinyi <1139077470@qq.com>
2023-07-27 10:35:48 +0800

    Revert "ADM-519 feat: finishing the design of error page"
    This reverts commit fdcfa86a3d665e1cb64d3b98b4db821b83f57bb0.

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,34 +1,10 @@
 import React from 'react'
-import styled from '@emotion/styled'
-import Header from '@src/layouts/Header'
-import { useNavigate } from 'react-router-dom'
-import { GuideButton } from '@src/components/Common/Buttons'
-import { Stack } from '@mui/material'
-
-const ErrorMessageContainer = styled.div({
-  padding: '48px',
-})
+import { ErrorMessage } from '@src/components/ErrorMessage'
 
 const ErrorPage = () => {
-  const navigate = useNavigate()
-
-  const backToHomePage = () => {
-    navigate('/home')
-  }
-
   return (
     <>
-      <Header />
-      <Stack direction='column' alignItems='center' flex={'auto'}>
-        <ErrorMessageContainer>
-          <h1>Oh no!</h1>
-          <p>
-            Something is not quite right. We promise we are working on it - in the meantime, perhaps head back to our
-            homepage and try again.
-          </p>
-        </ErrorMessageContainer>
-        <GuideButton onClick={backToHomePage}>Go to homepage</GuideButton>
-      </Stack>
+      <ErrorMessage />
     </>
   )
 }
