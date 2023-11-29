755b7c6544191d419220a3447a622efa77eebf1a
xuebing <xuebing.li@thoughtworks.com>
2023-11-29 11:34:55 +0800

    ADM-646: [frontend] feat: add type for diffHBStatus

--- frontend/src/context/Metrics/metricsSlice.tsx
+++ frontend/src/context/Metrics/metricsSlice.tsx
@@ -1,5 +1,5 @@
 /* istanbul ignore file */
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, current } from '@reduxjs/toolkit'
 import camelCase from 'lodash.camelcase'
 import { RootState } from '@src/store'
 import {
@@ -117,16 +117,19 @@ const findDifferentHBStatus = (
   importedCycleTimeSettingsValues: { [key: string]: string }[],
   jiraColumns: IVerifyJiraColumns[]
 ): string[] => {
-  let diffHBStatus = []
+  let diffHBStatus: string[] = []
   const addHBStatusMapping = jiraColumns.map((item) => {
     const addHBStatuses = item.value.statuses.map((status) => {
+      const HBStatusObj = importedCycleTimeSettingsValues.find(
+        (cycleTimeItem) => Object.keys(cycleTimeItem)[0] === status
+      )
       return {
         status,
-        HBStatus: importedCycleTimeSettingsValues.find((cycleTimeItem) => Object.keys(cycleTimeItem)[0] === status),
+        HBStatus: Object.values(HBStatusObj)[0],
       }
     })
 
-    const removeDuplicateHBStatus = []
+    const removeDuplicateHBStatus: string[] = []
     addHBStatuses
       .map((item) => item.HBStatus)
       .forEach((status) => {
@@ -297,7 +300,10 @@ export const metricsSlice = createSlice({
         const metricsContainsValues = Object.values(METRICS_CONSTANTS)
         const importedKeyMismatchWarning = compareArrays(importedCycleTimeSettingsKeys, jiraColumnsNames)
         const importedValueMismatchWarning = findDifferentValues(importedCycleTimeSettingsValues, metricsContainsValues)
-        const sameColumnButDiffHBStatusKeys = findDifferentHBStatus(importedCycleTimeSettingsValues, jiraColumns)
+        const sameColumnButDiffHBStatusKeys = findDifferentHBStatus(
+          importedCycleTime.importedCycleTimeSettings,
+          jiraColumns
+        )
 
         const getWarningMessage = (): string | null => {
           if (importedKeyMismatchWarning?.length) {
