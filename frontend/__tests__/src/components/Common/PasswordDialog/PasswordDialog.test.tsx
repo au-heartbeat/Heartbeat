1dc3e6d587be34ddba435fe7d0c85e0e2c3652fe
xuebing <xuebing.li@thoughtworks.com>
2023-12-13 09:28:46 +0800

    ADM-621: [frontend] feat: add unit test

--- frontend/__tests__/src/components/Common/PasswordDialog/PasswordDialog.test.tsx
+++ frontend/__tests__/src/components/Common/PasswordDialog/PasswordDialog.test.tsx
@@ -1,13 +1,13 @@
-import { render, act } from '@testing-library/react'
-import PasswordDialog from '@src/components/Common/PasswordDialog/index'
-import { SET_PASSWORD_TITLE, PASSWORD, ENCRYPTED_MESSAGE, ENCRYPT_CONFIRM } from '../../../fixtures'
-import React from 'react/index'
+import { act, render } from '@testing-library/react'
+import { CONFIRM_PASSWORD, ENCRYPT_CONFIRM, ENCRYPTED_MESSAGE, PASSWORD, SET_PASSWORD_TITLE } from '../../../fixtures'
 import userEvent from '@testing-library/user-event'
-
-const handleConfirm = jest.fn()
-const handleCancel = jest.fn()
+import PasswordDialog from '@src/components/Common/PasswordDialog'
+import React from 'react'
 
 describe('confirm dialog', () => {
+  beforeEach(() => jest.resetAllMocks())
+  const handleConfirm = jest.fn()
+  const handleCancel = jest.fn()
   const setup = () => {
     return render(
       <PasswordDialog isShowPasswordDialog={true} handleConfirm={handleConfirm} handleCancel={handleCancel} />
@@ -19,40 +19,100 @@ describe('confirm dialog', () => {
     expect(getByText(SET_PASSWORD_TITLE)).toBeInTheDocument()
   })
 
-  it('should password visibility', () => {
-    const { getByText, getByRole } = setup()
+  it('should call handleConfirm when clicking confirm', async () => {
+    const { getByText } = setup()
     const mockPassword = '123abc'
 
-    act(() => {
-      userEvent.click(getByRole('button', { name: 'toggle password visibility' }))
-      userEvent.type(getByText(PASSWORD), mockPassword)
+    await act(async () => {
+      await userEvent.type(getByText(PASSWORD), mockPassword)
+      await userEvent.type(getByText(CONFIRM_PASSWORD), mockPassword)
+      await userEvent.click(getByText(ENCRYPT_CONFIRM))
     })
-    setTimeout(() => {
-      expect(getByText(mockPassword)).toBeInTheDocument()
-    }, 100)
+
+    expect(handleConfirm).toBeCalledTimes(1)
   })
 
-  it('should show error for not match', () => {
-    const { getByText, getByRole } = setup()
-    const mockPassword = '123'
-
-    act(() => {
-      userEvent.click(getByRole('button', { name: 'toggle password visibility' }))
-      userEvent.type(getByText(PASSWORD), mockPassword)
-    })
-    setTimeout(() => {
-      expect(getByText(ENCRYPTED_MESSAGE.NOT_MATCH)).toBeInTheDocument()
-    }, 100)
-  })
-
-  it('should show error for password not black', () => {
+  it('should call handleCancel when clicking cancel button', async () => {
     const { getByText } = setup()
 
-    act(() => {
-      userEvent.click(getByText(ENCRYPT_CONFIRM))
+    await act(async () => {
+      await userEvent.click(getByText('Cancel'))
     })
-    setTimeout(() => {
-      expect(getByText(ENCRYPTED_MESSAGE.PASSWORD_EMPTY)).toBeInTheDocument()
-    }, 100)
+
+    expect(handleCancel).toBeCalledTimes(1)
+  })
+
+  it('should password visibility', async () => {
+    const { getByText, getByRole, getByLabelText } = setup()
+    const mockPassword = '123abc'
+    const input = getByLabelText(PASSWORD, { selector: 'input' })
+
+    await act(async () => {
+      await userEvent.click(getByRole('button', { name: 'toggle password visibility' }))
+      await userEvent.type(getByText(PASSWORD), mockPassword)
+    })
+
+    expect(input).toBeInTheDocument()
+    expect(input).toHaveValue(mockPassword)
+  })
+
+  it('should show error for not match', async () => {
+    const { getByText } = setup()
+    const mockPassword = '123'
+    const password = getByText(PASSWORD)
+
+    await act(async () => {
+      await userEvent.type(password, mockPassword)
+    })
+
+    expect(getByText(ENCRYPTED_MESSAGE.NOT_MATCH)).toBeInTheDocument()
+  })
+
+  it('should show error for password not blank', async () => {
+    const { getByText, getByLabelText } = setup()
+    const mockPrePassword = '123abc'
+    const input = getByLabelText(PASSWORD, { selector: 'input' })
+
+    await act(async () => {
+      await userEvent.type(getByText(PASSWORD), mockPrePassword)
+      await userEvent.type(getByText(CONFIRM_PASSWORD), mockPrePassword)
+      await userEvent.clear(input)
+    })
+
+    expect(getByText(ENCRYPT_CONFIRM)).toBeDisabled()
+    expect(getByText(ENCRYPTED_MESSAGE.PASSWORD_EMPTY)).toBeInTheDocument()
+  })
+
+  it('should show error for password not same', async () => {
+    const { getByText } = setup()
+    const mockPassword = '123aaa'
+    const mockConfirmPassword = '123bbb'
+    const confirmButton = getByText(ENCRYPT_CONFIRM)
+
+    expect(confirmButton).toBeDisabled()
+
+    await act(async () => {
+      await userEvent.type(getByText(PASSWORD), mockPassword)
+      await userEvent.type(getByText(CONFIRM_PASSWORD), mockConfirmPassword)
+      await userEvent.click(confirmButton)
+    })
+
+    expect(getByText(ENCRYPTED_MESSAGE.NOT_SAME)).toBeInTheDocument()
+  })
+
+  it('should enable confirm button', async () => {
+    const { getByText } = setup()
+    const mockPassword = '123aaa'
+    const mockConfirmPassword = '123aaa'
+    const confirmButton = getByText(ENCRYPT_CONFIRM)
+
+    expect(confirmButton).toBeDisabled()
+
+    await act(async () => {
+      await userEvent.type(getByText(PASSWORD), mockPassword)
+      await userEvent.type(getByText(CONFIRM_PASSWORD), mockConfirmPassword)
+    })
+
+    expect(confirmButton).toBeEnabled()
   })
 })
