b44b17fb196da32d0d0b52cbb5f57ebf9e7d3c84
GuangbinMa <guangbin.ma@thoughtworks.com>
2023-12-20 14:46:55 +0800

    ADM-684: [frontend] feat: add MetricsSection component

--- frontend/src/components/Metrics/ReportStep/index.tsx
+++ frontend/src/components/Metrics/ReportStep/index.tsx
@@ -3,7 +3,25 @@ import { useGenerateReportEffect } from '@src/hooks/useGenerateReportEffect'
 import { useNavigate } from 'react-router-dom'
 import { ErrorNotification } from '@src/components/ErrorNotification'
 import React from 'react'
-import { ErrorNotificationContainer } from '@src/components/Metrics/ReportStep/style'
+import {
+  ErrorNotificationContainer,
+  SectionContainer,
+  StyledLine,
+  StyledTitle,
+  StyledToggle,
+} from '@src/components/Metrics/ReportStep/style'
+
+const MetricsSection = (props: { title: string }) => (
+  <SectionContainer>
+    <StyledLine />
+    <StyledTitle>{props.title}</StyledTitle>
+    <StyledToggle>show more</StyledToggle>
+  </SectionContainer>
+)
+
+const shouldShowDoraMetrics = () => {
+  return true
+}
 
 const ReportStep = () => {
   const navigate = useNavigate()
@@ -15,6 +33,7 @@ const ReportStep = () => {
         navigate(ROUTE.ERROR_PAGE)
       ) : (
         <>
+          {shouldShowDoraMetrics() && <MetricsSection title={'DORA Metrics'} />}
           {reportErrorMsg && (
             <ErrorNotificationContainer>
               <ErrorNotification message={reportErrorMsg} />
--- frontend/src/components/Metrics/ReportStep/style.tsx
+++ frontend/src/components/Metrics/ReportStep/style.tsx
@@ -9,3 +9,33 @@ export const ErrorNotificationContainer = styled('div')({
   zIndex: Z_INDEX.MODAL_BACKDROP,
   width: '80%',
 })
+
+export const SectionContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  flexDirection: 'row',
+})
+
+export const StyledLine = styled('div')({
+  width: '0.25rem',
+  height: '1rem',
+  borderRight: '0.25rem solid #4350AF',
+  marginRight: '0.5rem',
+})
+
+export const StyledTitle = styled('div')({
+  color: '#000000D9',
+  fontFamily: 'Roboto',
+  fontSize: '1rem',
+  fontWeight: '600',
+  lineHeight: '3.25rem',
+  marginRight: '1.3rem',
+})
+
+export const StyledToggle = styled('div')({
+  color: '#4350AF',
+  fontFamily: 'Roboto',
+  fontSize: '0.75rem',
+  cursor: 'pointer',
+})
