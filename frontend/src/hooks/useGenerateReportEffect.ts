2444b2abf2ac42bafee55fd088528557f6626673
Zeng, Xiaoxing <xiaoxing.zeng@thoughtworks.com>
2023-12-22 19:31:19 +0800

    ADM-688:[frontend] feat: extract api call.

--- frontend/src/hooks/useGenerateReportEffect.ts
+++ frontend/src/hooks/useGenerateReportEffect.ts
@@ -1,42 +1,57 @@
 import { useRef, useState } from 'react'
 import { reportClient } from '@src/clients/report/ReportClient'
-import { ReportRequestDTO } from '@src/clients/report/dto/request'
+import { BoardReportRequestDTO, ReportRequestDTO } from '@src/clients/report/dto/request'
 import { UnknownException } from '@src/exceptions/UnkonwException'
 import { InternalServerException } from '@src/exceptions/InternalServerException'
-import { HttpStatusCode } from 'axios'
-import { pipelineReportMapper, sourceControlReportMapper } from '@src/hooks/reportMapper/report'
-import { ReportResponse, ReportResponseDTO } from '@src/clients/report/dto/response'
+import { ReportResponseDTO } from '@src/clients/report/dto/response'
 import { DURATION } from '@src/constants/commons'
 
 export interface useGenerateReportEffectInterface {
-  startPollingReports: (boardParams: ReportRequestDTO, doraParams: ReportRequestDTO) => void
+  startToRequestBoardData: (boardParams: BoardReportRequestDTO) => void
+  startToRequestDoraData: (doraParams: ReportRequestDTO) => void
   stopPollingReports: () => void
-  isBoardLoading: boolean
   isServerError: boolean
-  isPipelineLoading: boolean
-  isSourceControlLoading: boolean
   errorMessage: string
   reportData: ReportResponseDTO | undefined
 }
 
 export const useGenerateReportEffect = (): useGenerateReportEffectInterface => {
-  const [isBoardLoading, setIsBoardLoading] = useState(false)
-  const [isPipelineLoading, setIsPipelineLoading] = useState(false)
-  const [isSourceControlLoading, setIsSourceControlLoading] = useState(false)
   const [isServerError, setIsServerError] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
   const [reportData, setReportData] = useState<ReportResponseDTO>()
   const timerIdRef = useRef<NodeJS.Timer>()
+  let hasPollingStarted = false
 
-  const startPollingReports = (boardParams: ReportRequestDTO, doraParams: ReportRequestDTO) => {
-    setIsBoardLoading(true)
-    setIsPipelineLoading(true)
-    setIsSourceControlLoading(true)
-    Promise.race([
-      reportClient.retrieveReportByUrl(doraParams, '/dora-reports'),
-      reportClient.retrieveReportByUrl(boardParams, '/board-reports'),
-    ])
-      .then((res) => pollingReport(res.response.callbackUrl, res.response.interval))
+  const startToRequestBoardData = async (boardParams: ReportRequestDTO) => {
+    reportClient
+      .retrieveReportByUrl(boardParams, '/board-reports')
+      .then((res) => {
+        if (hasPollingStarted) return
+        hasPollingStarted = true
+        pollingReport(res.response.callbackUrl, res.response.interval)
+      })
+      .catch((e) => {
+        const err = e as Error
+        if (err instanceof InternalServerException || err instanceof UnknownException) {
+          setIsServerError(true)
+        } else {
+          setErrorMessage(`generate report: ${err.message}`)
+          setTimeout(() => {
+            setErrorMessage('')
+          }, DURATION.ERROR_MESSAGE_TIME)
+        }
+        stopPollingReports()
+      })
+  }
+
+  const startToRequestDoraData = async (doraParams: ReportRequestDTO) => {
+    reportClient
+      .retrieveReportByUrl(doraParams, '/dora-reports')
+      .then((res) => {
+        if (hasPollingStarted) return
+        hasPollingStarted = true
+        pollingReport(res.response.callbackUrl, res.response.interval)
+      })
       .catch((e) => {
         const err = e as Error
         if (err instanceof InternalServerException || err instanceof UnknownException) {
@@ -55,21 +70,14 @@ export const useGenerateReportEffect = (): useGenerateReportEffectInterface => {
     reportClient
       .pollingReport(url)
       .then((res: { status: number; response: ReportResponseDTO }) => {
-        const { sourceControlMetricsReady, pipelineMetricsReady, boardMetricsReady } = res.response
-        setReportData(res.response)
-        if (sourceControlMetricsReady) {
-          setIsSourceControlLoading(false)
-        }
-        if (pipelineMetricsReady) {
-          setIsPipelineLoading(false)
-        }
-        if (boardMetricsReady) {
-          setIsBoardLoading(false)
-        }
-        if (res.status === HttpStatusCode.Created) {
+        const response = res.response
+        setReportData(response)
+        if (response.allMetricsReady) {
           stopPollingReports()
         } else {
-          timerIdRef.current = setTimeout(() => pollingReport(url, interval), interval * 1000)
+          timerIdRef.current = setTimeout(() => {
+            pollingReport(url, interval)
+          }, interval * 1000)
         }
       })
       .catch((e) => {
@@ -87,15 +95,14 @@ export const useGenerateReportEffect = (): useGenerateReportEffectInterface => {
   }
 
   const stopPollingReports = () => {
+    hasPollingStarted = false
     clearInterval(timerIdRef.current)
   }
 
   return {
-    startPollingReports,
+    startToRequestBoardData,
+    startToRequestDoraData,
     stopPollingReports,
-    isPipelineLoading,
-    isSourceControlLoading,
-    isBoardLoading,
     reportData,
     isServerError,
     errorMessage,
