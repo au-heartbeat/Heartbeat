4caa6a2be17e0c45550c28549f5450439072c595
Nathan Wang <chao.wang3@thoughtworks.com>
2024-01-10 10:34:50 +0800

    ADM-714: [frontend]: feat: modify the logic of construct error result according to httpClient change.

--- frontend/src/clients/pipeline/PipelineToolClient.ts
+++ frontend/src/clients/pipeline/PipelineToolClient.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@src/clients/Httpclient'
-import axios, { HttpStatusCode, isAxiosError } from 'axios'
+import axios, { HttpStatusCode } from 'axios'
+import { isHeartBeatException } from '@src/exceptions'
+import { IHeartBeatException } from '@src/exceptions/ExceptionType'
 import { PipelineRequestDTO } from '@src/clients/pipeline/dto/request'
 import { IPipelineInfoResponseDTO } from '@src/clients/pipeline/dto/response'
 import { ERROR_CASE_TEXT_MAPPING, PIPELINE_TOOL_ERROR_MESSAGE, UNKNOWN_ERROR_TITLE } from '@src/constants/resources'
@@ -48,9 +50,10 @@ export class PipelineToolClient extends HttpClient {
       result.code = response.status
     } catch (e) {
       console.error(`Failed to get pipeline tool info for ${params.type}`, e)
-      if (isAxiosError(e)) {
-        result.code = e.response?.status
-        result.errorTitle = ERROR_CASE_TEXT_MAPPING[`${e.response?.status}`] || UNKNOWN_ERROR_TITLE
+      if (isHeartBeatException(e)) {
+        const exception = e as IHeartBeatException
+        result.code = exception.code
+        result.errorTitle = ERROR_CASE_TEXT_MAPPING[`${exception.code}`] || UNKNOWN_ERROR_TITLE
       } else {
         result.errorTitle = UNKNOWN_ERROR_TITLE
       }
