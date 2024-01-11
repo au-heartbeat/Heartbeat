8d986b7ff42d34c54ab135e51e0b207d254be67f
GuangbinMa <guangbin.ma@thoughtworks.com>
2024-01-11 18:13:22 +0800

    ADM-699: [frontend] test: prompt test coverage for ReportCard component

--- frontend/__tests__/src/components/Common/ReportGrid/ReportCard.test.tsx
+++ frontend/__tests__/src/components/Common/ReportGrid/ReportCard.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { ReportCard } from '@src/components/Common/ReportGrid/ReportCard';
+import clearAllMocks = jest.clearAllMocks;
 
 describe('Report Card', () => {
+  afterEach(() => {
+    clearAllMocks();
+  });
+
   it('should not show exceeding items', () => {
     const items = [
       {
@@ -24,5 +29,15 @@ describe('Report Card', () => {
     expect(screen.getByText('1.00')).toBeInTheDocument();
     expect(screen.getByText('2.00')).toBeInTheDocument();
     expect(screen.queryByText('3.00')).not.toBeInTheDocument();
+  });
+
+  it('should show error message when errorMessage is not empty', () => {
+    const errorMessage = 'Data loading failed';
+
+    const { getByText, queryByText } = render(
+      <ReportCard title={'card'} items={null} xs={6} errorMessage={errorMessage} />
+    );
+
+    expect(screen.getByText('Data loading failed')).toBeInTheDocument();
   });
 });
