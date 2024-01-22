3b50a2a9b96fcb5ffa55b82010dbc7703fa19cd4
Kai Zhou <kai.zhou@thoughtworks.com>
2024-01-22 17:53:46 +0800

    [kai.zhou][adm-718]: chroe: format code

--- frontend/src/containers/MetricsStep/index.tsx
+++ frontend/src/containers/MetricsStep/index.tsx
@@ -48,8 +48,6 @@ const MetricsStep = () => {
     cycleTimeSettings.filter((e) => e.value === DONE).length > 1;
   const { getBoardInfo, isLoading, errorMessage } = useGetBoardInfoEffect();
 
-  console.log(errorMessage, 'error message is');
-
   const getInfo = () => {
     getBoardInfo(boardConfig).then((res) => {
       if (res.data) {
