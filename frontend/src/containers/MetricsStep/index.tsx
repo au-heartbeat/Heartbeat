dafc760679ba051edc13418d497f6c6b88b7b13b
Kai Zhou <kai.zhou@thoughtworks.com>
2024-01-22 17:32:52 +0800

    [kai.zhou][adm-718]: test: fix some unit test

--- frontend/src/containers/MetricsStep/index.tsx
+++ frontend/src/containers/MetricsStep/index.tsx
@@ -13,12 +13,12 @@ import {
   MetricsSelectionTitle,
 } from '@src/containers/MetricsStep/style';
 import { DeploymentFrequencySettings } from '@src/containers/MetricsStep/DeploymentFrequencySettings';
+import { DeploymentFrequencySettings } from '@src/containers/MetricsStep/DeploymentFrequencySettings';
+import { selectCycleTimeSettings, selectMetricsContent } from '@src/context/Metrics/metricsSlice';
 import { selectMetricsContent, updateMetricsState } from '@src/context/Metrics/metricsSlice';
 import { CYCLE_TIME_SETTINGS_TYPES, DONE, REQUIRED_DATA } from '@src/constants/resources';
 import { closeAllNotifications } from '@src/context/notification/NotificationSlice';
 import { Classification } from '@src/containers/MetricsStep/Classification';
-import DateRangeViewer from '@src/components/Common/DateRangeViewer';
-import { useGetBoardInfoEffect } from '@src/hooks/useGetBoardInfo';
 import { CycleTime } from '@src/containers/MetricsStep/CycleTime';
 import { RealDone } from '@src/containers/MetricsStep/RealDone';
 import EmptyContent from '@src/components/Common/EmptyContent';
@@ -50,7 +50,6 @@ const MetricsStep = () => {
 
   /* istanbul ignore next */
   const getInfo = () => {
-    console.log(boardConfig);
     getBoardInfo(boardConfig).then((res) => {
       if (res.data) {
         dispatch(updateBoardVerifyState(true));
