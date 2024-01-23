5560a1ae88f50a7b82349f95176d6d6d5459726b
Shiqi Yuan <shiqi.yuan@thoughtworks.com>
2024-01-23 17:06:34 +0800

    ADM-739: [backend]test:add tests for missed code line in KanbanCsvServiceTest

--- backend/src/test/java/heartbeat/service/report/KanbanCsvServiceTest.java
+++ backend/src/test/java/heartbeat/service/report/KanbanCsvServiceTest.java
@@ -7,6 +7,7 @@ import heartbeat.client.dto.board.jira.JiraCardField;
 import heartbeat.client.dto.board.jira.Status;
 import heartbeat.controller.board.dto.response.CardCollection;
 import heartbeat.controller.board.dto.response.ColumnValue;
+import heartbeat.controller.board.dto.response.CycleTimeInfo;
 import heartbeat.controller.board.dto.response.JiraCardDTO;
 import heartbeat.controller.board.dto.response.JiraColumnDTO;
 import heartbeat.controller.board.dto.response.TargetField;
@@ -33,6 +34,7 @@ import java.util.List;
 
 import static heartbeat.service.report.BoardCsvFixture.MOCK_JIRA_CARD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
@@ -163,6 +165,58 @@ class KanbanCsvServiceTest {
 	}
 
 	@Test
+	void shouldSaveCsvWithoutOrderedNonDoneCardsByJiraColumnWhenNonDoneCardIsNotEmpty() throws URISyntaxException {
+		URI uri = new URI("site-uri");
+		when(urlGenerator.getUri(any())).thenReturn(uri);
+		when(jiraService.getJiraBoardConfig(any(), any(), any())).thenReturn(JiraBoardConfigDTO.builder().build());
+		when(jiraService.getJiraColumns(any(), any(), any())).thenReturn(JiraColumnResult.builder()
+			.jiraColumnResponse(List.of(
+					JiraColumnDTO.builder().value(ColumnValue.builder().statuses(List.of("Doing")).build()).build(),
+					JiraColumnDTO.builder().value(ColumnValue.builder().statuses(List.of("Blocked")).build()).build(),
+					JiraColumnDTO.builder().value(ColumnValue.builder().statuses(List.of("Testing")).build()).build(),
+					JiraColumnDTO.builder().value(ColumnValue.builder().statuses(List.of("DONE")).build()).build()))
+			.build());
+		JiraCardDTO jiraCardDTO = JiraCardDTO.builder().build();
+		JiraCardDTO blockedJiraCard = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder()
+				.fields(JiraCardField.builder().status(Status.builder().name("Blocked").build()).build())
+				.build())
+			.build();
+		JiraCardDTO doingJiraCard = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder()
+				.fields(JiraCardField.builder().status(Status.builder().name("Doing").build()).build())
+				.build())
+			.build();
+		JiraCardDTO testingJiraCard = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder()
+				.fields(JiraCardField.builder().status(Status.builder().name("Testing").build()).build())
+				.build())
+			.build();
+		List<JiraCardDTO> NonDoneJiraCardDTOList = new ArrayList<>() {
+			{
+				add(blockedJiraCard);
+				add(doingJiraCard);
+				add(testingJiraCard);
+			}
+		};
+		kanbanCsvService.generateCsvInfo(
+				GenerateReportRequest.builder()
+					.jiraBoardSetting(JiraBoardSetting.builder()
+						.targetFields(List.of(TargetField.builder().name("Doing").build(),
+								TargetField.builder().name("Done").build()))
+						.build())
+					.build(),
+				CardCollection.builder().jiraCardDTOList(List.of(jiraCardDTO)).build(),
+				CardCollection.builder().jiraCardDTOList(NonDoneJiraCardDTOList).build());
+
+		verify(csvFileGenerator).convertBoardDataToCSV(jiraCardDTOCaptor.capture(), anyList(), any(), any());
+		assertEquals(5, jiraCardDTOCaptor.getValue().size());
+		assertEquals(blockedJiraCard, jiraCardDTOCaptor.getValue().get(2));
+		assertEquals(doingJiraCard, jiraCardDTOCaptor.getValue().get(3));
+		assertEquals(testingJiraCard, jiraCardDTOCaptor.getValue().get(4));
+	}
+
+	@Test
 	void shouldSaveCsvWithOrderedNonDoneCardsByJiraColumnDescendingWhenNonDoneCardIsNotEmptyAndStatusIsNull()
 			throws URISyntaxException {
 		URI uri = new URI("site-uri");
@@ -213,6 +267,88 @@ class KanbanCsvServiceTest {
 
 	@Test
 	void shouldAddFixedFieldsWhenItIsNotInSettingsFields() throws URISyntaxException {
+		URI uri = new URI("site-uri");
+		when(urlGenerator.getUri(any())).thenReturn(uri);
+		when(jiraService.getJiraBoardConfig(any(), any(), any())).thenReturn(JiraBoardConfigDTO.builder().build());
+		when(jiraService.getJiraColumns(any(), any(), any())).thenReturn(JiraColumnResult.builder()
+			.jiraColumnResponse(List
+				.of(JiraColumnDTO.builder().value(ColumnValue.builder().statuses(List.of("BLOCKED")).build()).build()))
+			.build());
+		JiraCardDTO jiraCardDTO = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder().fields(MOCK_JIRA_CARD()).build())
+			.build();
+		JiraCardDTO blockedJiraCard = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder().fields(MOCK_JIRA_CARD()).build())
+			.build();
+		List<JiraCardDTO> NonDoneJiraCardDTOList = new ArrayList<>() {
+			{
+				add(blockedJiraCard);
+			}
+		};
+		kanbanCsvService.generateCsvInfo(
+				GenerateReportRequest.builder()
+					.jiraBoardSetting(JiraBoardSetting.builder()
+						.targetFields(List.of(
+								TargetField.builder().name("assignee").flag(true).key("key-assignee").build(),
+								TargetField.builder().name("fake-target1").flag(true).key("key-target1").build(),
+								TargetField.builder().name("fake-target2").flag(false).key("key-target2").build()))
+						.build())
+					.build(),
+				CardCollection.builder().jiraCardDTOList(List.of(jiraCardDTO)).build(),
+				CardCollection.builder().jiraCardDTOList(NonDoneJiraCardDTOList).build());
+
+		verify(csvFileGenerator).convertBoardDataToCSV(anyList(), csvFieldsCaptor.capture(),
+				csvNewFieldsCaptor.capture(), any());
+		assertEquals(22, csvFieldsCaptor.getValue().size());
+		BoardCSVConfig targetValue = csvNewFieldsCaptor.getValue().get(0);
+		assertEquals("baseInfo.fields.customFields.key-target1", targetValue.getValue());
+		assertEquals("fake-target1", targetValue.getLabel());
+		assertEquals("key-target1", targetValue.getOriginKey());
+	}
+
+	@Test
+	void shouldAddFixedFieldsWhenItIsNotInSettingsFieldsAndCardHasOriginCycleTime() throws URISyntaxException {
+		URI uri = new URI("site-uri");
+		when(urlGenerator.getUri(any())).thenReturn(uri);
+		when(jiraService.getJiraBoardConfig(any(), any(), any())).thenReturn(JiraBoardConfigDTO.builder().build());
+		when(jiraService.getJiraColumns(any(), any(), any())).thenReturn(JiraColumnResult.builder()
+			.jiraColumnResponse(List
+				.of(JiraColumnDTO.builder().value(ColumnValue.builder().statuses(List.of("BLOCKED")).build()).build()))
+			.build());
+		JiraCardDTO jiraCardDTO = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder().fields(MOCK_JIRA_CARD()).build())
+			.build();
+		JiraCardDTO blockedJiraCard = JiraCardDTO.builder()
+			.baseInfo(JiraCard.builder().fields(MOCK_JIRA_CARD()).build())
+			.originCycleTime(List.of(CycleTimeInfo.builder().column("BLOCKED").day(30.7859).build()))
+			.build();
+		List<JiraCardDTO> NonDoneJiraCardDTOList = new ArrayList<>() {
+			{
+				add(blockedJiraCard);
+			}
+		};
+		kanbanCsvService.generateCsvInfo(
+				GenerateReportRequest.builder()
+					.jiraBoardSetting(JiraBoardSetting.builder()
+						.targetFields(List.of(
+								TargetField.builder().name("assignee").flag(true).key("key-assignee").build(),
+								TargetField.builder().name("fake-target1").flag(true).key("key-target1").build(),
+								TargetField.builder().name("fake-target2").flag(false).key("key-target2").build()))
+						.build())
+					.build(),
+				CardCollection.builder().jiraCardDTOList(List.of(jiraCardDTO)).build(),
+				CardCollection.builder().jiraCardDTOList(NonDoneJiraCardDTOList).build());
+
+		verify(csvFileGenerator).convertBoardDataToCSV(anyList(), csvFieldsCaptor.capture(), anyList(), any());
+		assertEquals(23, csvFieldsCaptor.getValue().size());
+		BoardCSVConfig targetValue = csvFieldsCaptor.getValue().get(22);
+		assertEquals("cycleTimeFlat.BLOCKED", targetValue.getValue());
+		assertEquals("OriginCycleTime: BLOCKED", targetValue.getLabel());
+		assertNull(targetValue.getOriginKey());
+	}
+
+	@Test
+	void shouldAddFixedFieldsWhen() throws URISyntaxException {
 		URI uri = new URI("site-uri");
 		when(urlGenerator.getUri(any())).thenReturn(uri);
 		when(jiraService.getJiraBoardConfig(any(), any(), any())).thenReturn(JiraBoardConfigDTO.builder().build());
