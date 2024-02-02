b5d09379cdf1c32c49a2bcbd37f65d4ff13ea075
Kai Zhou <kai.zhou@thoughtworks.com>
2024-02-02 16:47:44 +0800

    [kai.zhou][adm-718]: add retry button for board metircs

--- frontend/src/hooks/useGetBoardInfo.tsx
+++ frontend/src/hooks/useGetBoardInfo.tsx
@@ -1,9 +1,10 @@
+import { StyledRetryButton } from '@src/components/Metrics/MetricsStep/DeploymentFrequencySettings/PresentationForErrorCases/style';
 import { BOARD_CONFIG_INFO_ERROR, BOARD_CONFIG_INFO_TITLE } from '@src/constants/resources';
 import { boardInfoClient } from '@src/clients/board/BoardInfoClient';
 import { BoardInfoRequestDTO } from '@src/clients/board/dto/request';
-import { getJiraBoardToken } from '@src/utils/util';
+import { HEARTBEAT_EXCEPTION_CODE } from '@src/constants/resources';
+import { ReactNode, useState } from 'react';
 import { AxiosResponse } from 'axios';
-import { useState } from 'react';
 import get from 'lodash/get';
 
 export type JiraColumns = Record<string, string>[];
@@ -20,36 +21,45 @@ export interface useGetBoardInfoInterface {
   errorMessage: Record<string, string>;
 }
 
-const codeMapping = {
-  400: {
-    title: BOARD_CONFIG_INFO_TITLE.INVALID_INPUT,
-    message: BOARD_CONFIG_INFO_ERROR.NOT_FOUND,
-  },
-  401: {
-    title: BOARD_CONFIG_INFO_TITLE.UNAUTHORIZED_REQUEST,
-    message: BOARD_CONFIG_INFO_ERROR.NOT_FOUND,
-  },
-  403: {
-    title: BOARD_CONFIG_INFO_TITLE.FORBIDDEN_REQUEST,
-    message: BOARD_CONFIG_INFO_ERROR.FORBIDDEN,
-  },
-  404: {
-    title: BOARD_CONFIG_INFO_TITLE.NOT_FOUND,
-    message: BOARD_CONFIG_INFO_ERROR.NOT_FOUND,
-  },
+const codeMapping = (code: string | number, retry: ReactNode) => {
+  const codes = {
+    400: {
+      title: BOARD_CONFIG_INFO_TITLE.INVALID_INPUT,
+      message: BOARD_CONFIG_INFO_ERROR.NOT_FOUND,
+    },
+    401: {
+      title: BOARD_CONFIG_INFO_TITLE.UNAUTHORIZED_REQUEST,
+      message: BOARD_CONFIG_INFO_ERROR.NOT_FOUND,
+    },
+    403: {
+      title: BOARD_CONFIG_INFO_TITLE.FORBIDDEN_REQUEST,
+      message: BOARD_CONFIG_INFO_ERROR.FORBIDDEN,
+    },
+    404: {
+      title: BOARD_CONFIG_INFO_TITLE.NOT_FOUND,
+      message: BOARD_CONFIG_INFO_ERROR.NOT_FOUND,
+    },
+    [HEARTBEAT_EXCEPTION_CODE.TIMEOUT]: {
+      title: BOARD_CONFIG_INFO_TITLE.EMPTY,
+      message: (
+        <>
+          {BOARD_CONFIG_INFO_ERROR.RETRY} {retry}
+        </>
+      ),
+    },
+  };
+  return get(codes, code);
 };
 
 export const useGetBoardInfoEffect = (): useGetBoardInfoInterface => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState({});
+
   const getBoardInfo = (data: BoardInfoRequestDTO) => {
     setIsLoading(true);
     setErrorMessage({});
     return boardInfoClient
-      .getBoardInfo({
-        ...data,
-        token: getJiraBoardToken(data.token, data.email),
-      })
+      .getBoardInfo(data)
       .then((res) => {
         if (!res.data) {
           setErrorMessage({
@@ -61,7 +71,9 @@ export const useGetBoardInfoEffect = (): useGetBoardInfoInterface => {
       })
       .catch((err) => {
         const { code } = err;
-        setErrorMessage(get(codeMapping, code, {}));
+        setErrorMessage(
+          codeMapping(code, <StyledRetryButton onClick={() => getBoardInfo(data)}>Retry</StyledRetryButton>),
+        );
         return err;
       })
       .finally(() => setIsLoading(false));
