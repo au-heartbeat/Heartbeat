97b1af8bb75786014c6be97110a6588b3afc132f
Nathan Wang <chao.wang3@thoughtworks.com>
2024-02-04 13:07:09 +0800

    ADM-731: [frontend]: refactor the <Classification /> to fix the scenario that targetFields names duplicated.

--- frontend/src/containers/MetricsStep/Classification/index.tsx
+++ frontend/src/containers/MetricsStep/Classification/index.tsx
@@ -1,42 +1,69 @@
+import { TypedStyledAutocompleted, ITargetFieldType } from '@src/components/Common/MultiAutoComplete/styles';
 import { saveTargetFields, selectClassificationWarningMessage } from '@src/context/Metrics/metricsSlice';
 import { MetricsSettingTitle } from '@src/components/Common/MetricsSettingTitle';
 import { WarningNotification } from '@src/components/Common/WarningNotification';
-import MultiAutoComplete from '@src/components/Common/MultiAutoComplete';
+import { Checkbox, createFilterOptions, TextField } from '@mui/material';
 import { useAppDispatch } from '@src/hooks/useAppDispatch';
+import { Z_INDEX } from '@src/constants/commons';
 import { useAppSelector } from '@src/hooks';
 import React, { useMemo } from 'react';
 
-export interface TargetFieldType {
-  name: string;
-  key: string;
-  flag: boolean;
-}
 export interface classificationProps {
   title: string;
   label: string;
-  targetFields: TargetFieldType[];
+  targetFields: ITargetFieldType[];
 }
+
+export const suffixForDuplicateNames = (targetFields: ITargetFieldType[]) => {
+  const countMap = {} as { [key: string]: number };
+  type TTargetFieldTypeMeta = [ITargetFieldType, number, boolean?];
+  let metas: TTargetFieldTypeMeta[] = [];
+
+  for (const targetField of targetFields) {
+    if (countMap[targetField.name]) {
+      const count = ++countMap[targetField.name];
+      const meta = [targetField, count] as [ITargetFieldType, number];
+      metas.push(meta);
+    } else {
+      countMap[targetField.name] = 1;
+      const meta = [targetField, 1] as [ITargetFieldType, number];
+      metas.push(meta);
+    }
+  }
+
+  metas = metas.map((meta) => {
+    const [{ name }] = meta;
+    const isDuplicated = metas.filter(([{ name: anotherName }]) => anotherName === name).length > 1;
+    return [...meta, isDuplicated] as unknown as TTargetFieldTypeMeta;
+  });
+
+  return metas.map(([targetField, count, isDuplicated]) =>
+    isDuplicated ? { ...targetField, name: `${targetField.name}-${count}` } : { ...targetField },
+  );
+};
 
 export const Classification = ({ targetFields, title, label }: classificationProps) => {
   const dispatch = useAppDispatch();
+  const targetFieldsWithSuffix = suffixForDuplicateNames(targetFields);
   const classificationWarningMessage = useAppSelector(selectClassificationWarningMessage);
-  const options = targetFields.map(({ name }) => name);
-  const classificationSettings = targetFields.filter(({ flag }) => flag).map(({ name }) => name);
+  const selectedOptions = targetFieldsWithSuffix.filter(({ flag }) => flag);
   const isAllSelected = useMemo(() => {
-    return classificationSettings.length > 0 && classificationSettings.length === targetFields.length;
-  }, [classificationSettings, targetFields.length]);
+    return selectedOptions.length > 0 && selectedOptions.length === targetFieldsWithSuffix.length;
+  }, [selectedOptions]);
 
-  const handleChange = (_: React.SyntheticEvent, value: string[]) => {
-    const newClassificationSettings =
-      value[value.length - 1] === 'All'
-        ? isAllSelected
-          ? []
-          : targetFields.map((targetField) => targetField.name)
-        : [...value];
+  const handleChange = (_: React.SyntheticEvent, value: ITargetFieldType[]) => {
+    let nextSelectedOptions: ITargetFieldType[];
+    if (value.length === 0) {
+      nextSelectedOptions = [];
+    } else {
+      nextSelectedOptions =
+        value[value.length - 1].key === 'all' ? (isAllSelected ? [] : [...targetFieldsWithSuffix]) : [...value];
+    }
     const updatedTargetFields = targetFields.map((targetField) => ({
       ...targetField,
-      flag: newClassificationSettings.includes(targetField.name),
+      flag: !!nextSelectedOptions.find((option) => option.key === targetField.key),
     }));
+
     dispatch(saveTargetFields(updatedTargetFields));
   };
 
@@ -44,13 +71,41 @@ export const Classification = ({ targetFields, title, label }: classificationPro
     <>
       <MetricsSettingTitle title={title} />
       {classificationWarningMessage && <WarningNotification message={classificationWarningMessage} />}
-      <MultiAutoComplete
-        optionList={options}
-        selectedOption={classificationSettings}
-        textFieldLabel={label}
-        isError={false}
-        onChangeHandler={handleChange}
-        isSelectAll={isAllSelected}
+      <TypedStyledAutocompleted
+        multiple
+        options={targetFieldsWithSuffix}
+        disableCloseOnSelect
+        value={selectedOptions}
+        filterOptions={(options, params): ITargetFieldType[] => {
+          const filtered = createFilterOptions<ITargetFieldType>()(options, params);
+          const allOption = {
+            flag: isAllSelected,
+            name: 'All',
+            key: 'all',
+          };
+          return [allOption, ...filtered];
+        }}
+        getOptionLabel={(option) => option.name}
+        onChange={(event, value) => handleChange(event, value as ITargetFieldType[])}
+        renderOption={(props, option: any, state) => {
+          const selectAllProps = option.key === 'all' ? { checked: isAllSelected } : {};
+          return (
+            <li {...props}>
+              <Checkbox style={{ marginRight: 8 }} checked={state.selected} {...selectAllProps} />
+              {option.name as string}
+            </li>
+          );
+        }}
+        renderInput={(params) => (
+          <TextField {...params} required={true} error={false} variant='standard' label={label} />
+        )}
+        slotProps={{
+          popper: {
+            sx: {
+              zIndex: Z_INDEX.DROPDOWN,
+            },
+          },
+        }}
       />
     </>
   );
