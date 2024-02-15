cfc1b1abaed5dac6f915d2cd7388a221215264e1
guzhongren <guzhongren@live.cn>
2024-02-15 23:03:34 +0800

    build image

--- frontend/e2e/specs/createANewProject.spec.ts
+++ frontend/e2e/specs/createANewProject.spec.ts
@@ -26,9 +26,7 @@ test('Create a new project', async ({ homePage, configStep, metricsStep }) => {
     startDate: dayjs(configStepData.dateRange.startDate).format('MM/DD/YYYY'),
     endDate: dayjs(configStepData.dateRange.endDate).format('MM/DD/YYYY'),
   };
-  console.log(process.env.APP_ORIGIN);
-  console.log(process.env.E2E_TOKEN_JIRA?.substring(1, 5));
-  console.log(dayjs.locale());
+  console.log(JSON.stringify(configStep));
   console.log(`dateRange: ${JSON.stringify(dateRange)}`);
 
   await homePage.goto();
