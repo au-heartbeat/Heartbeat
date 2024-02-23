e6a3c8887424bff614be99161ff22155aad95c0c
Simon Tal <simon_tal@hotmail.com>
2024-02-23 11:31:11 +0800

    ADM-785:[Frontend] remove unused testing case

--- frontend/e2e/specs/snapshot-testing.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '../fixtures/testWithExtendFixtures';
-import { expect } from '@playwright/test';
-
-test('Snapshot testing', async ({ page, homePage }) => {
-  await homePage.goto();
-  await homePage.importProjectFromFile();
-  await page.waitForLoadState('networkidle');
-  await expect(page).toHaveScreenshot('metrics-page.png', { fullPage: true });
-});
