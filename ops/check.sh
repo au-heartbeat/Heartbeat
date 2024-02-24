8040e6bd08ba425dab90e911c997faba93eb771b
Simon Tal <simon_tal@hotmail.com>
2024-02-24 20:37:14 +0800

    ADM-785:[Frontend] fix e2e report upload

--- ops/check.sh
+++ ops/check.sh
@@ -165,8 +165,9 @@ e2e_container_check() {
     -e "E2E_TOKEN_GITHUB=${E2E_TOKEN_GITHUB:-}" \
     -e "CI=${CI:-}" \
     heartbeat_e2e:latest \
-    pnpm run e2e:major-ci && sleep infinity
+    pnpm run e2e:major-ci && tar -zcvf ./e2e-reports.tar.gz ./e2e/reports
 
+  docker cp hb_e2e_runner:/app/e2e-reports.tar.gz ./e2e-reports.tar.gz
   docker rm hb_e2e_runner
 }
 
