41705eca88f2273cdb38c2bc6cdf918354764e1e
zhoukai <zhoukai@zhous-MacBook-Pro.local>
2024-03-04 16:26:31 +0800

    [kai.zhou][adm-818]: handle hide loading button when pipeline info get failed

--- frontend/src/context/Metrics/metricsSlice.ts
+++ frontend/src/context/Metrics/metricsSlice.ts
@@ -379,7 +379,7 @@ export const metricsSlice = createSlice({
           .filter((pipeline: pipeline) => pipeline.orgName.toLowerCase() === organization.toLowerCase())
           .map((item: pipeline) => item.name);
       const getValidPipelines = (pipelines: IPipelineConfig[]) =>
-        pipelines.length > 0
+        pipelines.length && pipelines.filter(({ id }) => id).length
           ? pipelines.map(({ id, organization, pipelineName, step, branches }) => ({
               id,
               organization: orgNames.find((i) => (i as string).toLowerCase() === organization.toLowerCase()) || '',
@@ -407,7 +407,8 @@ export const metricsSlice = createSlice({
       };
 
       const getPipelinesWarningMessage = (pipelines: IPipelineConfig[]) => {
-        if (!pipelines.length || isProjectCreated) {
+        const hasPipeline = pipelines.filter(({ id }) => id).length;
+        if (!pipelines.length || isProjectCreated || !hasPipeline) {
           return [];
         }
         return pipelines.map((pipeline) => createPipelineWarning(pipeline));
@@ -415,6 +416,7 @@ export const metricsSlice = createSlice({
 
       const deploymentSettings =
         state.deploymentFrequencySettings.length > 0 ? state.deploymentFrequencySettings : importedDeployment;
+
       state.deploymentFrequencySettings = getValidPipelines(deploymentSettings);
       state.deploymentWarningMessage = getPipelinesWarningMessage(deploymentSettings);
     },
