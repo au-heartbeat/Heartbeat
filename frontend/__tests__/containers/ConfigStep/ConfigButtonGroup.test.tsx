d38f1265e4bc217c7475452276e1ce4cd4219628
yp.wu <yiping.wu@yipings-MacBook-Pro.local>
2024-03-11 16:55:54 +0800

    AMD-793 [frontend] test: add test for config button group component

---
+++ frontend/__tests__/containers/ConfigStep/ConfigButtonGroup.test.tsx
@@ -0,0 +1,28 @@
+import { ConfigButtonGrop } from '@src/containers/ConfigStep/ConfigButton';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+describe('ConfigButtonGroup', () => {
+  const setup = (isVerified: boolean, isLoading: boolean, isHBTimeOut: boolean, isDisableVerifyButton: boolean) => {
+    return render(
+      <ConfigButtonGrop
+        isHBTimeOut={isHBTimeOut}
+        isVerified={isVerified}
+        isLoading={isLoading}
+        isDisableVerifyButton={isDisableVerifyButton}
+      />,
+    );
+  };
+
+  it('renders a disabled VerifyButton with "Verified" text when isVerified is true and isLoading is false', () => {
+    setup(true, false, false, false); // Use a different moduleType for clarity
+    expect(screen.getByText('Verified')).toBeInTheDocument();
+    expect(screen.getByText('Reset')).toBeInTheDocument();
+    expect(screen.getByText('Verified')).toBeDisabled();
+  });
+  it('renders a VerifyButton with "Reverify" text and submit type when isHBTimeOut is true', () => {
+    setup(false, false, true, false); // Use a different moduleType for clarity
+    expect(screen.getByText('Reverify')).toBeInTheDocument();
+    expect(screen.getByText('Reverify')).toHaveAttribute('type', 'submit');
+  });
+});
