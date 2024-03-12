837bb7601e66c916fc64df83c06923a8456df161
Leiqiuhong <qiuhong.lei@thoughtworks.com>
2024-03-12 17:00:58 +0800

    ADM-691: [frontend] feat: add multi options logic

--- frontend/src/containers/MetricsStep/ReworkSettings/style.tsx
+++ frontend/src/containers/MetricsStep/ReworkSettings/style.tsx
@@ -11,6 +11,7 @@ export const StyledLink = styled(Link)({
 export const ReworkHeaderWrapper = styled('div')({
   display: 'flex',
   gap: '1rem',
+  marginTop: '1rem',
 });
 
 export const ReworkSettingsWrapper = styled('div')`
@@ -19,7 +20,7 @@ export const ReworkSettingsWrapper = styled('div')`
   border: ${theme.main.cardBorder};
   box-shadow: none;
   margin-bottom: 1rem;
-  line-height: '2rem';
+  line-height: 2rem;
   boarder-radius: 0.25rem;
   padding: 2.5%;
   box-sizing: border-box;
