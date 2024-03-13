5ea0c7afdd1944b465b58f12641410b2f1be84d3
Genhao Liu <genhao.liu@thoughtworks.com>
2024-03-13 15:28:00 +0800

    ADM-692:[backend] test: throw exception when calculate rework given analyse state in rework times info list

--- backend/src/test/java/heartbeat/service/report/ReworkFixture.java
+++ backend/src/test/java/heartbeat/service/report/ReworkFixture.java
@@ -6,6 +6,7 @@ import heartbeat.controller.board.dto.response.ReworkTimesInfo;
 
 import java.util.List;
 
+import static heartbeat.controller.board.dto.request.CardStepsEnum.ANALYSE;
 import static heartbeat.controller.board.dto.request.CardStepsEnum.BLOCK;
 import static heartbeat.controller.board.dto.request.CardStepsEnum.DEVELOPMENT;
 import static heartbeat.controller.board.dto.request.CardStepsEnum.DONE;
@@ -17,6 +18,19 @@ public class ReworkFixture {
 
 	public static CardCollection MOCK_CARD_COLLECTION() {
 		List<ReworkTimesInfo> reworkTimesInfos = List.of(ReworkTimesInfo.builder().state(TODO).times(1).build(),
+				ReworkTimesInfo.builder().state(DEVELOPMENT).times(1).build(),
+				ReworkTimesInfo.builder().state(BLOCK).times(1).build(),
+				ReworkTimesInfo.builder().state(WAITING).times(1).build(),
+				ReworkTimesInfo.builder().state(REVIEW).times(1).build(),
+				ReworkTimesInfo.builder().state(DONE).times(1).build());
+		List<JiraCardDTO> jiraCardList = List.of(JiraCardDTO.builder().reworkTimesInfos(reworkTimesInfos).build(),
+				JiraCardDTO.builder().reworkTimesInfos(reworkTimesInfos).build());
+		return CardCollection.builder().reworkCardNumber(2).reworkRatio(1).jiraCardDTOList(jiraCardList).build();
+	}
+
+	public static CardCollection MOCK_CARD_COLLECTION_WITH_ANALYSE() {
+		List<ReworkTimesInfo> reworkTimesInfos = List.of(ReworkTimesInfo.builder().state(TODO).times(1).build(),
+				ReworkTimesInfo.builder().state(ANALYSE).times(1).build(),
 				ReworkTimesInfo.builder().state(DEVELOPMENT).times(1).build(),
 				ReworkTimesInfo.builder().state(BLOCK).times(1).build(),
 				ReworkTimesInfo.builder().state(WAITING).times(1).build(),
