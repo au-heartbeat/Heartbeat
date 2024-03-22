8aabbcce1a44e432d37913a9e340d9c00ff6c80b
Nathan Wang <chao.wang3@thoughtworks.com>
2024-03-22 15:30:09 +0800

    [ADM-877][frontend]: feat: add range amount limitation.

--- frontend/src/constants/resources.ts
+++ frontend/src/constants/resources.ts
@@ -423,7 +423,6 @@ export const AUTHORIZE_ORGANIZATION_LINK =
 
 export const DEFAULT_MONTH_INTERVAL_DAYS = 30;
 export const TIME_RANGE_TITLE = 'Time range settings';
-
 export const ADD_TIME_RANGE_BUTTON_TEXT = 'New time range';
-
 export const REMOVE_BUTTON_TEXT = 'Remove';
+export const MAX_TIME_RANGE_AMOUNT = 6;
--- frontend/src/containers/ConfigStep/DateRangePicker/DateRangePickerGroup.tsx
+++ frontend/src/containers/ConfigStep/DateRangePicker/DateRangePickerGroup.tsx
@@ -1,15 +1,16 @@
 import { DateRangePickerGroupContainer, AddButton } from '@src/containers/ConfigStep/DateRangePicker/style';
 import { DateRangePicker } from '@src/containers/ConfigStep/DateRangePicker/DateRangePicker';
+import { ADD_TIME_RANGE_BUTTON_TEXT, MAX_TIME_RANGE_AMOUNT } from '@src/constants/resources';
 import { selectDateRange, updateDateRange } from '@src/context/config/configSlice';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { useAppDispatch, useAppSelector } from '@src/hooks/useAppDispatch';
-import { ADD_TIME_RANGE_BUTTON_TEXT } from '@src/constants/resources';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Add } from '@mui/icons-material';
 
 export const DateRangePickerGroup = () => {
   const dispatch = useAppDispatch();
   const dateRangeGroup = useAppSelector(selectDateRange);
+  const isAddButtonDisabled = dateRangeGroup.length === MAX_TIME_RANGE_AMOUNT;
   const addRangeHandler = () => {
     const newDateRangeGroup = dateRangeGroup.map((range) => ({ ...range })).concat({ startDate: null, endDate: null });
     dispatch(updateDateRange(newDateRangeGroup));
@@ -20,7 +21,7 @@ export const DateRangePickerGroup = () => {
         {dateRangeGroup.map(({ startDate, endDate }, index) => (
           <DateRangePicker startDate={startDate} endDate={endDate} index={index} key={index} />
         ))}
-        <AddButton startIcon={<Add />} onClick={addRangeHandler}>
+        <AddButton disabled={isAddButtonDisabled} startIcon={<Add />} onClick={addRangeHandler}>
           {ADD_TIME_RANGE_BUTTON_TEXT}
         </AddButton>
       </LocalizationProvider>
