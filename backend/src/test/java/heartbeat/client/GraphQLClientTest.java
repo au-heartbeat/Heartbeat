f149a1a6d21952a0ba2e252fbb5002a19212a28f
shuaiwang <shuai.wang@thoughtworks.com>
2024-03-25 17:21:11 +0800

    ADM-872[backend] feat: fix dot star error

--- backend/src/test/java/heartbeat/client/GraphQLClientTest.java
+++ backend/src/test/java/heartbeat/client/GraphQLClientTest.java
@@ -24,7 +24,8 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
