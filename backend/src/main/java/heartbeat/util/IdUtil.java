3c9892d585d6e8f1733ac0bee4e9e15ffd420c4e
Shiqi Yuan <shiqi.yuan@thoughtworks.com>
2024-03-26 14:27:02 +0800

    ADM-873: [backend] test: delete unused method

--- backend/src/main/java/heartbeat/util/IdUtil.java
+++ backend/src/main/java/heartbeat/util/IdUtil.java
@@ -4,8 +4,6 @@ public interface IdUtil {
 
 	String BOARD_REPORT_PREFIX = "board-";
 
-	String DORA_REPORT_PREFIX = "dora-";
-
 	String PIPELINE_REPORT_PREFIX = "pipeline-";
 
 	String SOURCE_CONTROL_PREFIX = "sourceControl-";
@@ -14,10 +12,6 @@ public interface IdUtil {
 
 	static String getBoardReportId(String timeStamp) {
 		return BOARD_REPORT_PREFIX + timeStamp;
-	}
-
-	static String getDoraReportId(String timeStamp) {
-		return DORA_REPORT_PREFIX + timeStamp;
 	}
 
 	static String getPipelineReportId(String timeStamp) {
