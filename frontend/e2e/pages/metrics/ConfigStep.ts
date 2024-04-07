a0d829eeee929aaac2428354ada9af52343b5108
guzhongren <guzhongren@live.cn>
2024-04-07 22:15:17 +0800

    ADM-751 refactor

--- frontend/e2e/pages/metrics/ConfigStep.ts
+++ frontend/e2e/pages/metrics/ConfigStep.ts
@@ -9,7 +9,7 @@ interface IBoardData {
   boardId: string;
   email: string;
   site: string;
-  token: string | undefined;
+  token: string;
 }
 
 interface IPipelineToolData {
@@ -253,7 +253,7 @@ export class ConfigStep {
     await this.boardTokenInput.fill(token!);
   }
 
-  async fillAndverifyBoardConfig(boardData: IBoardData) {
+  async fillAndVerifyBoardConfig(boardData: IBoardData) {
     await this.fillBoardConfigForm(boardData);
 
     await expect(this.boardVerifyButton).toBeEnabled();
