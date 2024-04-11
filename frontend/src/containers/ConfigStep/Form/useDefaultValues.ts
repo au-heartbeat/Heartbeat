22b14d6da44ca28cfe247783763e1d16e9839967
Nathan Wang <chao.wang3@thoughtworks.com>
2024-04-11 10:26:48 +0800

    [ADM-887][frontend]: chore: refine code.

--- frontend/src/containers/ConfigStep/Form/useDefaultValues.ts
+++ frontend/src/containers/ConfigStep/Form/useDefaultValues.ts
@@ -1,4 +1,10 @@
 import {
+  CALENDAR_TYPE_LITERAL,
+  BOARD_TYPE_LITERAL,
+  PIPELINE_TOOL_TYPE_LITERAL,
+  SOURCE_CONTROL_TYPE_LITERAL,
+} from '@src/containers/ConfigStep/Form/literal';
+import {
   IBasicInfoData,
   IBoardConfigData,
   IPipelineToolData,
@@ -10,12 +16,12 @@ import { useAppSelector } from '@src/hooks/useAppDispatch';
 export const basicInfoDefaultValues: IBasicInfoData = {
   projectName: '',
   dateRange: [],
-  calendarType: 'Regular Calendar(Weekend Considered)',
+  calendarType: CALENDAR_TYPE_LITERAL[0],
   metrics: [],
 };
 
 export const boardConfigDefaultValues: IBoardConfigData = {
-  type: 'Jira',
+  type: BOARD_TYPE_LITERAL[0],
   boardId: '',
   email: '',
   site: '',
@@ -23,12 +29,12 @@ export const boardConfigDefaultValues: IBoardConfigData = {
 };
 
 export const pipelineToolDefaultValues: IPipelineToolData = {
-  type: 'BuildKite',
+  type: PIPELINE_TOOL_TYPE_LITERAL[0],
   token: '',
 };
 
 export const sourceControlDefaultValues: ISourceControlData = {
-  type: 'GitHub',
+  type: SOURCE_CONTROL_TYPE_LITERAL[0],
   token: '',
 };
 
