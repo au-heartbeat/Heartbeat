52970ec6067b0c1a166e9eedd53e6d974831f753
bomu <ruiqing.piao@thoughtworks.com>
2024-04-15 10:54:12 +0800

    ADM-889:[frontend] fix: rename column title as 'subtitle'

--- frontend/src/containers/ReportStep/ReportDetail/dora.tsx
+++ frontend/src/containers/ReportStep/ReportDetail/dora.tsx
@@ -1,6 +1,6 @@
 import { ReportDataWithThreeColumns, ReportDataWithTwoColumns } from '@src/hooks/reportMapper/reportUIDataStructure';
+import { METRICS_TITLE, PIPELINE_STEP, SUBTITLE } from '@src/constants/resources';
 import ReportForThreeColumns from '@src/components/Common/ReportForThreeColumns';
-import { METRICS_TITLE, NAME, PIPELINE_STEP } from '@src/constants/resources';
 import ReportForTwoColumns from '@src/components/Common/ReportForTwoColumns';
 import { ReportResponseDTO } from '@src/clients/report/dto/response';
 import { reportMapper } from '@src/hooks/reportMapper/report';
@@ -17,7 +17,7 @@ const showTwoColumnSection = (title: string, value: Optional<ReportDataWithTwoCo
   value && <ReportForTwoColumns title={title} data={value} />;
 
 const showThreeColumnSection = (title: string, value: Optional<ReportDataWithThreeColumns[]>) =>
-  value && <ReportForThreeColumns title={title} fieldName={PIPELINE_STEP} listName={NAME} data={value} />;
+  value && <ReportForThreeColumns title={title} fieldName={PIPELINE_STEP} listName={SUBTITLE} data={value} />;
 
 export const DoraDetail = withGoBack(({ data }: Property) => {
   const mappedData = reportMapper(data);
