3109db5f587e327e6beea100966f0339847ace52
PengxiWPix <pengxi.wang@thoughtworks.com>
2024-04-17 10:29:02 +0800

    ADM-927:[frontend]feat: add retry button

--- frontend/src/containers/MetricsStep/index.tsx
+++ frontend/src/containers/MetricsStep/index.tsx
@@ -110,7 +110,8 @@ const MetricsStep = () => {
       {isShowCrewsAndRealDone && (
         <MetricSelectionWrapper>
           {isLoading && <Loading />}
-          <MetricsSelectionTitle>Board configuration</MetricsSelectionTitle>
+          <MetricsSelectionTitle>Board configuration {<StyledRetryButton onClick={getInfo}>Retry</StyledRetryButton>} </MetricsSelectionTitle>
+
           {isEmpty(errorMessage) ? (
             <>
               <Crews options={users} title={'Crew settings'} label={'Included Crews'} />
@@ -154,7 +155,7 @@ const MetricsStep = () => {
         requiredData.includes(REQUIRED_DATA.LEAD_TIME_FOR_CHANGES) ||
         requiredData.includes(REQUIRED_DATA.DEV_MEAN_TIME_TO_RECOVERY)) && (
         <MetricSelectionWrapper aria-label='Pipeline Configuration Section'>
-          <MetricsSelectionTitle>Pipeline configuration</MetricsSelectionTitle>
+          <MetricsSelectionTitle>Pipeline configuration {<StyledRetryButton onClick={getInfo}>Retry</StyledRetryButton>} </MetricsSelectionTitle>
           <DeploymentFrequencySettings />
         </MetricSelectionWrapper>
       )}
