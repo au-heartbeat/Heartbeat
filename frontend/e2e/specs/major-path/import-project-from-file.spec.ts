a357ed9fe9f3f357c9f375c2219d51923bda4212
Nathan Wang <chao.wang3@thoughtworks.com>
2024-05-20 15:26:26 +0800

    [ADM-906]: feat: refactor the `import-from-file` scenario for multiple ranges.

--- frontend/e2e/specs/major-path/import-project-from-file.spec.ts
+++ frontend/e2e/specs/major-path/import-project-from-file.spec.ts
@@ -1,6 +1,13 @@
 import {
   BOARD_METRICS_RESULT,
   FLAG_AS_BLOCK_PROJECT_BOARD_METRICS_RESULT,
+  BOARD_METRICS_RESULT_MULTIPLE_RANGES,
+  BOARD_METRICS_VELOCITY_MULTIPLE_RANGES,
+  BOARD_METRICS_CYCLETIME_MULTIPLE_RANGES,
+  BOARD_METRICS_CLASSIFICATION_MULTIPLE_RANGES,
+  BOARD_METRICS_REWORK_MULTIPLE_RANGES,
+  BAORD_CSV_COMPARED_LINES,
+  DORA_METRICS_RESULT_MULTIPLE_RANGES,
 } from '../../fixtures/create-new/report-result';
 import { cycleTimeByStatusFixture } from '../../fixtures/cycle-time-by-status/cycle-time-by-status-fixture';
 import { importMultipleDoneProjectFromFile } from '../../fixtures/import-file/multiple-done-config-file';
@@ -28,38 +35,41 @@ test('Import project from file', async ({ homePage, configStep, metricsStep, rep
   await configStep.clickPreviousButtonAndClickCancelThenRemainPage();
   await configStep.verifyAllConfig();
   await configStep.goToMetrics();
-  await metricsStep.waitForShown();
 
-  // To verify board configuration matches json file data
+  await metricsStep.waitForShown();
   await metricsStep.checkCrewsAreChanged(importMultipleDoneProjectFromFile.crews);
   await metricsStep.checkLastAssigneeCrewFilterChecked();
   await metricsStep.checkCycleTimeSettingIsByColumn();
   await metricsStep.checkHeartbeatStateIsSet(hbStateData, true);
-
   await metricsStep.selectCycleTimeSettingsType(cycleTimeByStatusFixture.cycleTime.type);
   await metricsStep.checkHeartbeatStateIsSet(hbStateDataEmptyByStatus, false);
   await metricsStep.selectHeartbeatState(hbStateData, false);
   await metricsStep.checkHeartbeatStateIsSet(hbStateData, false);
-
   await metricsStep.selectCycleTimeSettingsType(importMultipleDoneProjectFromFile.cycleTime.type);
   await metricsStep.checkHeartbeatStateIsSet(hbStateDataEmptyByStatus, true);
   await metricsStep.selectHeartbeatState(hbStateData, true);
   await metricsStep.checkHeartbeatStateIsSet(hbStateData, true);
   await metricsStep.selectGivenPipelineCrews(importMultipleDoneProjectFromFile.pipelineCrews);
-
   await metricsStep.selectReworkSettings(metricsStepData.reworkTimesSettings);
-
   await metricsStep.checkClassifications(importMultipleDoneProjectFromFile.classification);
   await metricsStep.checkPipelineConfigurationAreChanged(importMultipleDoneProjectFromFile.deployment);
 
   await metricsStep.goToReportPage();
   await reportStep.confirmGeneratedReport();
-  await reportStep.checkBoardMetrics(BOARD_METRICS_RESULT);
-  // todo
-  // await reportStep.checkBoardMetricsDetails(ProjectCreationType.IMPORT_PROJECT_FROM_FILE, 9);
-  // todo
-  // await reportStep.checkDoraMetricsDetails(ProjectCreationType.IMPORT_PROJECT_FROM_FILE);
-  await reportStep.checkDownloadReports();
+  await reportStep.checkBoardMetricsForMultipleRanges(BOARD_METRICS_RESULT_MULTIPLE_RANGES);
+  await reportStep.checkBoardMetricsDetailsForMultipleRanges({
+    projectCreationType: ProjectCreationType.CREATE_A_NEW_PROJECT,
+    velocityData: BOARD_METRICS_VELOCITY_MULTIPLE_RANGES,
+    cycleTimeData: BOARD_METRICS_CYCLETIME_MULTIPLE_RANGES,
+    classificationData: BOARD_METRICS_CLASSIFICATION_MULTIPLE_RANGES,
+    reworkData: BOARD_METRICS_REWORK_MULTIPLE_RANGES,
+    csvCompareLines: BAORD_CSV_COMPARED_LINES,
+  });
+  await reportStep.checkDoraMetricsDetailsForMultipleRanges({
+    doraMetricsReportData: DORA_METRICS_RESULT_MULTIPLE_RANGES,
+    projectCreationType: ProjectCreationType.CREATE_A_NEW_PROJECT,
+  });
+  await reportStep.checkMetricDownloadDataForMultipleRanges(3);
 });
 
 test('Import project from flag as block and without block column', async ({
