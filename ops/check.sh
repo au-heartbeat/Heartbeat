9eeddee3765e2b63f01f56cec975f3fa2633b01c
Mandy-Tang <hitangman@qq.com>
2024-05-31 09:27:23 +0800

    ADM-944: [frontend] feat: change hex check to exclude just the chart-result.ts file

--- ops/check.sh
+++ ops/check.sh
@@ -229,7 +229,7 @@ hex_check() {
   result=$(grep -rinE \
     --exclude-dir='node_modules' \
     --exclude-dir='coverage' \
-    --exclude-dir='e2e/fixtures' \
+    --exclude='chart-result.ts' \
     --exclude='*.html' \
     --exclude='*.svg' \
     --exclude='*.xml' \
