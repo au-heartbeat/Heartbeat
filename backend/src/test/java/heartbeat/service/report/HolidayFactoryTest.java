eeb8f4b1e673230cb2f0de5208bb9001a1ab0ae9
zhou-yinyuan <yinyuan.zhou@thoughtworks.com>
2024-06-17 17:24:27 +0800

    ADM-952 [backend]: fix the format

--- backend/src/test/java/heartbeat/service/report/HolidayFactoryTest.java
+++ backend/src/test/java/heartbeat/service/report/HolidayFactoryTest.java
@@ -9,7 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
