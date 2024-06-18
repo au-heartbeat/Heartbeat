e7a7dccde300c54fb5652aa0e45389fc206657db
zhou-yinyuan <yinyuan.zhou@thoughtworks.com>
2024-06-18 15:19:39 +0800

    ADM-952 [backend][frontend]: finish all

---
+++ backend/src/main/java/heartbeat/service/report/AbstractCountryHoliday.java
@@ -0,0 +1,33 @@
+package heartbeat.service.report;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import heartbeat.controller.report.dto.request.CalendarTypeEnum;
+import heartbeat.exception.DecodeCalendarException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@Slf4j
+public abstract class AbstractCountryHoliday {
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	abstract public Map<String, Boolean> loadHolidayList(String year);
+
+	protected <T> T decoder(String value, CalendarTypeEnum calendarTypeEnum, String year, Class<T> clazz) {
+		try {
+			return objectMapper.readValue(value, clazz);
+		}
+		catch (JsonProcessingException e) {
+			String msg = String.format("error decode %s-%s value to %s", year, calendarTypeEnum, clazz);
+			log.error(msg);
+			throw new DecodeCalendarException(msg, 500);
+		}
+	}
+
+}
---
+++ backend/src/main/java/heartbeat/service/report/VietnamHoliday.java
@@ -0,0 +1,39 @@
+package heartbeat.service.report;
+
+import heartbeat.client.CalendarificFeignClient;
+import heartbeat.client.dto.board.jira.CalendarificHolidayResponseDTO;
+import heartbeat.controller.report.dto.request.CalendarTypeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class VietnamHoliday extends AbstractCountryHoliday {
+
+	@Autowired
+	private CalendarificFeignClient calendarificFeignClient;
+
+	public Map<String, Boolean> loadHolidayList(String year) {
+		Map<String, Boolean> holidayMap = new HashMap<>();
+		log.info("Start to get vietnam holiday by year: {}", year);
+
+		List<CalendarificHolidayResponseDTO.Response.CalendarificHolidayDetail> holidays = decoder(
+				calendarificFeignClient.getHolidays(CalendarTypeEnum.VN.getValue().toLowerCase(), year),
+				CalendarTypeEnum.VN, year, CalendarificHolidayResponseDTO.class)
+			.getResponse()
+			.getHolidays();
+		log.info("Successfully get vietnam holiday list:{}", holidays);
+
+		holidays.forEach(holiday -> {
+			String date = holiday.getDate().getIso();
+			holidayMap.put(date, holiday.getType().contains("National holiday"));
+		});
+		return holidayMap;
+	}
+
+}
