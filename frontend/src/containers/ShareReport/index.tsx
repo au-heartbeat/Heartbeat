176aebdafab34c158839c5ffaafe91d96d1ad90f
Mandy-Tang <hitangman@qq.com>
2024-07-02 16:41:21 +0800

    ADM-973 [frontend] feat: add Share Report Page

---
+++ frontend/src/containers/ShareReport/index.tsx
@@ -0,0 +1,27 @@
+import {
+  csvTimeStamp,
+  dateRanges,
+  handleSave,
+  metrics,
+  reportInfos,
+  startToRequestBoardData,
+  startToRequestDoraData,
+} from './mock';
+import ReportContent from '../ReportStep/ReportContent';
+import { useParams } from 'react-router-dom';
+
+const ShareReport = () => {
+  const { reportId } = useParams();
+  const reportContentProps = {
+    metrics,
+    dateRanges,
+    startToRequestDoraData,
+    startToRequestBoardData,
+    reportInfos,
+    handleSave,
+    csvTimeStamp,
+  };
+  return <ReportContent {...reportContentProps} />;
+};
+
+export default ShareReport;
