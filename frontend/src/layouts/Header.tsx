673f9be8b1f87b10e1b20bba4283a7484c4c5b04
Mandy-Tang <hitangman@qq.com>
2024-07-11 10:56:36 +0800

    ADM-973 [frontend] feat: fix sonar issues

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -59,7 +59,7 @@ const Header = () => {
       <IconContainer>
         <ShareReportTrigger />
         {shouldShowHomeIcon() && (
-          <span title='Home' onClick={goHome} onKeyDown={goHome} aria-label={'Home'} role='button'>
+          <span title='Home' onClick={goHome} aria-label={'Home'}>
             <HomeIcon />
           </span>
         )}
