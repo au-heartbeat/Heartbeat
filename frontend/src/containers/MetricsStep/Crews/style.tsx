71bc4a54a17dea688a687f472216fd619197dbcc
zhou-yinyuan <yinyuan.zhou@thoughtworks.com>
2024-08-01 15:20:46 +0800

    ADM-967 [frontend]: responsible the metrics page

--- frontend/src/containers/MetricsStep/Crews/style.tsx
+++ frontend/src/containers/MetricsStep/Crews/style.tsx
@@ -1,10 +1,19 @@
 import { styled } from '@mui/material/styles';
 import { RadioGroup } from '@mui/material';
+import { theme } from '@src/theme';
 
 export const StyledRadioGroup = styled(RadioGroup)({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)',
+  display: 'flex',
+  flexDirection: 'row',
+  '& > *': {
+    flexBasis: '25%',
+    margin: 0,
+  },
   paddingTop: '1rem',
+  [theme.breakpoints.down('md')]: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
 });
 
 export const WarningMessage = styled('span')({
