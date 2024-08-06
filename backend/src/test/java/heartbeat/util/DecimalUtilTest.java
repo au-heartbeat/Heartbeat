066f36f90d5913913ace8470aa8222561bb05944
zhou-yinyuan <yinyuan.zhou@thoughtworks.com>
2024-08-06 16:49:06 +0800

    ADM-989 [backend]: fix sonar issues

--- backend/src/test/java/heartbeat/util/DecimalUtilTest.java
+++ backend/src/test/java/heartbeat/util/DecimalUtilTest.java
@@ -1,10 +1,7 @@
 package heartbeat.util;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 class DecimalUtilTest {
 
