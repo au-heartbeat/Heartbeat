8f5df0d76b95c8f8f392ff43d12c19bee72df6f6
guzhongren <guzhongren@live.cn>
2024-08-28 09:53:24 +0800

    pre-commit pre-tag

--- frontend/scripts/git-hooks/pre-tag.sh
+++ frontend/scripts/git-hooks/pre-tag.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
-echo "Before creating a tag, please confirm if you have completed the update of the backend API version."
+pushed_tags=$(git push --dry-run --porcelain | grep '^refs/tags/' | awk '{print $2}')
 
-echo "Have you completed the backend API version update? (y/n): \c"
-read -r answer
+if [ -n "$pushed_tags" ]; then
+    echo "Before creating a tag, please confirm if you have completed the update of the backend API version."
 
-if [ "$answer" != "y" ] && [ "$answer" != "Y" ]; then
-    echo "Please complete the backend API version update before creating a tag."
-    exit 1
+    echo "Have you completed the backend API version update? (y/n): \c"
+    read -r answer
+
+    if [ "$answer" != "y" ] && [ "$answer" != "Y" ]; then
+        echo "Please complete the backend API version update before creating a tag."
+        exit 1
+    fi
 fi
 
 exit 0
